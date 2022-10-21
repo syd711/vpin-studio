2ec60a793797f7f294a8d32b4a830ef2e90c5584
Matthias Faust <syd711@gmail.com>
2022-10-21 09:57:23 +0200

    Layout

---
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/ToolbarController.java
@@ -0,0 +1,19 @@
+package de.mephisto.vpin.ui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ToolbarController implements Initializable {
+
+
+  // Add a public no-args constructor
+  public ToolbarController() {
+  }
+
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+  }
+}
