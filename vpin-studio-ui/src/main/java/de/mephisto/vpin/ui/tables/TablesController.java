702b3891c031174867f586e06dbf94b81629e842
Matthias Faust <syd711@gmail.com>
2022-11-04 10:00:41 +0100

    Splitted tables view

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/TablesController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/TablesController.java
@@ -1,20 +1,14 @@
 package de.mephisto.vpin.ui.tables;
 
-import de.mephisto.vpin.restclient.PopperScreen;
 import de.mephisto.vpin.restclient.VPinStudioClient;
-import de.mephisto.vpin.restclient.representations.GameMediaItemRepresentation;
-import de.mephisto.vpin.restclient.representations.GameMediaRepresentation;
 import de.mephisto.vpin.restclient.representations.GameRepresentation;
 import de.mephisto.vpin.ui.NavigationController;
 import de.mephisto.vpin.ui.StudioFXController;
-import de.mephisto.vpin.ui.util.BindingUtil;
 import de.mephisto.vpin.ui.util.MediaUtil;
 import de.mephisto.vpin.ui.util.ValidationTexts;
 import de.mephisto.vpin.ui.util.WidgetFactory;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -22,20 +16,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.media.Media;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Paint;
 import org.apache.commons.lang3.StringUtils;
@@ -43,12 +31,7 @@ import org.kordamp.ikonli.javafx.FontIcon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.util.List;
 import java.util.*;
 
 public class TablesController implements Initializable, StudioFXController {
@@ -91,103 +74,19 @@ public class TablesController implements Initializable, StudioFXController {
   private TextField textfieldSearch;
 
   @FXML
-  private BorderPane screenTopper;
-
-  @FXML
-  private BorderPane screenBackglass;
-
-  @FXML
-  private BorderPane screenDMD;
-
-  @FXML
-  private BorderPane screenPlayfield;
-
-  @FXML
-  private BorderPane screenApron;
-
-  @FXML
-  private BorderPane screenOther2;
-
-  @FXML
-  private BorderPane screenWheel;
-
-  @FXML
-  private BorderPane screenInfo;
-
-  @FXML
-  private BorderPane screenHelp;
-
-  @FXML
-  private BorderPane screenLoading;
-
-  @FXML
-  private BorderPane screenAudio;
-
-  @FXML
-  private BorderPane screenAudioLaunch;
-
-  @FXML
-  private Button playfieldViewBtn;
-
-  @FXML
-  private Label labelTableCount;
-
-  @FXML
-  private Accordion accordion;
-
-  @FXML
-  private TitledPane titledPaneMedia;
-
-  @FXML
-  private Label labelId;
-
-  @FXML
-  private Label labelRom;
-
-  @FXML
-  private Label labelRomAlias;
-
-  @FXML
-  private Label labelNVOffset;
-
-  @FXML
-  private Label labelFilename;
-
-  @FXML
-  private Label labelLastPlayed;
-
-  @FXML
-  private Label labelTimesPlayed;
-
-  @FXML
-  private Label labelHSFilename;
-
-  @FXML
-  private Slider volumeSlider;
-
-  @FXML
   private Label validationErrorLabel;
 
   @FXML
   private Node validationError;
 
   @FXML
-  private TextArea highscoreTextArea;
+  private Label labelTableCount;
 
   @FXML
-  private ImageView rawDirectB2SImage;
+  private Parent tablesSideBar;
 
   @FXML
-  private Button openDirectB2SImageButton;
-
-  @FXML
-  private Button editHsFileNameBtn;
-
-  @FXML
-  private Button editRomNameBtn;
-
-  @FXML
-  private Label resolutionLabel;
+  private TablesSidebarController tablesSideBarController; //fxml magic!
 
   // Add a public no-args constructor
   public TablesController() {
@@ -224,37 +123,6 @@ public class TablesController implements Initializable, StudioFXController {
   }
 
   @FXML
-  private void onMediaViewClick(ActionEvent e) {
-    Button source = (Button) e.getSource();
-    BorderPane borderPane = (BorderPane) source.getParent();
-    Node center = borderPane.getCenter();
-    if (center == null) {
-      center = screenPlayfield.getCenter();
-    }
-
-
-    if (center instanceof MediaView) {
-      MediaView mediaView = (MediaView) center;
-      Media media = mediaView.getMediaPlayer().getMedia();
-      String s = media.getSource();
-      try {
-        Desktop.getDesktop().browse(URI.create(s));
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
-    }
-    else if (center instanceof ImageView) {
-      ImageView imageView = (ImageView) center;
-      String url = (String) imageView.getUserData();
-      try {
-        Desktop.getDesktop().browse(URI.create(url));
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  @FXML
   private void onSearchKeyPressed(KeyEvent e) {
     if (e.getCode().equals(KeyCode.ENTER)) {
       tableView.getSelectionModel().select(0);
@@ -282,17 +150,6 @@ public class TablesController implements Initializable, StudioFXController {
   private void onTablesScan() {
     WidgetFactory.createProgressDialog(new TablesScanProgressModel(client, "Scanning Tables"));
     this.onReload();
-  }
-
-  @FXML
-  private void onRomEdit() {
-    GameRepresentation gameRepresentation = tableView.getSelectionModel().selectedItemProperty().get();
-    String romName = WidgetFactory.showInputDialog("Enter ROM Name", null, gameRepresentation.getRom());
-    if (romName != null) {
-      gameRepresentation.setRom(romName);
-      client.saveGame(gameRepresentation);
-      this.onReload();
-    }
   }
 
   @FXML
@@ -328,18 +185,7 @@ public class TablesController implements Initializable, StudioFXController {
   }
 
   @FXML
-  private void onHsFileNameEdit() {
-    GameRepresentation gameRepresentation = tableView.getSelectionModel().selectedItemProperty().get();
-    String fs = WidgetFactory.showInputDialog("EM Highscore Filename", "Enter the name of the highscore file for this table.\nThe file is located in the 'User' folder.", gameRepresentation.getHsFileName());
-    if (fs != null) {
-      gameRepresentation.setHsFileName(fs);
-      client.saveGame(gameRepresentation);
-      this.onReload();
-    }
-  }
-
-  @FXML
-  private void onReload() {
+  public void onReload() {
     GameRepresentation gameRepresentation = tableView.getSelectionModel().selectedItemProperty().get();
     List<GameRepresentation> games = client.getGames();
     List<GameRepresentation> filtered = new ArrayList<>();
@@ -358,7 +204,7 @@ public class TablesController implements Initializable, StudioFXController {
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     client = new VPinStudioClient();
-    this.accordion.setExpandedPane(titledPaneMedia);
+    tablesSideBarController.setTablesController(this);
 
     bindTable();
     bindSearchField();
@@ -484,198 +330,26 @@ public class TablesController implements Initializable, StudioFXController {
     if (!data.isEmpty()) {
       tableView.getSelectionModel().select(0);
     }
-
-    volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-      @Override
-      public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-        GameRepresentation game = tableView.getSelectionModel().selectedItemProperty().get();
-        if(game != null) {
-          BindingUtil.debouncer.debounce("tableVolume" + game.getId(), () -> {
-            int value = t1.intValue();
-            if (value == 0) {
-              value = 1;
-            }
-            game.setVolume(value);
-            client.saveGame(game);
-          }, 1000);
-        }
-      }
-    });
-
-    titledPaneMedia.expandedProperty().addListener(new ChangeListener<Boolean>() {
-      @Override
-      public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean expanded) {
-        GameRepresentation game = tableView.getSelectionModel().selectedItemProperty().get();
-        if (expanded) {
-          refreshView(Optional.of(game));
-        }
-        else {
-          resetMedia();
-        }
-      }
-    });
-  }
-
-  private void resetMedia() {
-    disposeMediaPane(screenAudioLaunch);
-    disposeMediaPane(screenAudio);
-    disposeMediaPane(screenLoading);
-    disposeMediaPane(screenHelp);
-    disposeMediaPane(screenInfo);
-    disposeMediaPane(screenDMD);
-    disposeMediaPane(screenBackglass);
-    disposeMediaPane(screenTopper);
-    disposeMediaPane(screenApron);
-    disposeMediaPane(screenPlayfield);
-    disposeMediaPane(screenOther2);
-    disposeMediaPane(screenWheel);
   }
 
   private void refreshView(Optional<GameRepresentation> g) {
-    editHsFileNameBtn.setDisable(g.isEmpty());
-    editRomNameBtn.setDisable(g.isEmpty());
-
+    tablesSideBarController.setGame(g);
+    GameRepresentation game = g.get();
     if (g.isPresent()) {
-      GameRepresentation game = g.get();
-      GameMediaRepresentation gameMedia = client.getGameMedia(game.getId());
-
-      volumeSlider.setDisable(false);
-      volumeSlider.setValue(game.getVolume());
-
-      GameMediaItemRepresentation item = gameMedia.getItem(PopperScreen.PlayField);
-      playfieldViewBtn.setVisible(item != null);
-
-      labelId.setText(String.valueOf(game.getId()));
-      labelRom.setText(game.getOriginalRom() != null ? game.getOriginalRom() : game.getRom());
-      labelRomAlias.setText(game.getOriginalRom() != null ? game.getRom() : "-");
-      labelNVOffset.setText(game.getNvOffset() > 0 ? String.valueOf(game.getNvOffset()) : "-");
-      labelFilename.setText(game.getGameFileName());
-      labelLastPlayed.setText(game.getLastPlayed() != null ? game.getLastPlayed().toString() : "-");
-      labelTimesPlayed.setText(String.valueOf(game.getNumberPlays()));
-      labelHSFilename.setText(game.getHsFileName());
-
-      refreshDirectB2SPreview(g);
-
       validationError.setVisible(game.getValidationState() > 0);
       if (game.getValidationState() > 0) {
         validationErrorLabel.setText(ValidationTexts.getValidationMessage(game));
       }
-
-      if (titledPaneMedia.isExpanded()) {
-        refreshMedia(gameMedia, game);
-      }
-
-      String rawHighscore = game.getRawHighscore();
-      if (rawHighscore != null) {
-        highscoreTextArea.setText(rawHighscore);
-      }
-      else {
-        highscoreTextArea.setText("");
-      }
-
       NavigationController.setBreadCrumb(Arrays.asList("Tables", game.getGameDisplayName()));
     }
     else {
-      resetMedia();
-
-      playfieldViewBtn.setVisible(false);
-
-      volumeSlider.setValue(100);
-      volumeSlider.setDisable(true);
-
-      labelId.setText("-");
-      labelRom.setText("-");
-      labelRomAlias.setText("-");
-      labelNVOffset.setText("-");
-      labelFilename.setText("-");
-      labelLastPlayed.setText("-");
-      labelTimesPlayed.setText("-");
-      labelHSFilename.setText("-");
-
-      refreshDirectB2SPreview(Optional.empty());
-
-      highscoreTextArea.setText("");
-
       validationError.setVisible(false);
       NavigationController.setBreadCrumb(Arrays.asList("Tables"));
     }
   }
 
-  private void refreshDirectB2SPreview(Optional<GameRepresentation> game) {
-    try {
-      openDirectB2SImageButton.setVisible(false);
-      openDirectB2SImageButton.setTooltip(new Tooltip("Open directb2s image"));
-      rawDirectB2SImage.setVisible(false);
-
-      if(game.isPresent()) {
-        InputStream input = client.getDirectB2SImage(game.get());
-        javafx.scene.image.Image image = new Image(input);
-        rawDirectB2SImage.setVisible(true);
-        rawDirectB2SImage.setImage(image);
-        input.close();
-
-        if (image.getWidth() > 300) {
-          openDirectB2SImageButton.setVisible(true);
-          resolutionLabel.setText("Resolution: " + (int) image.getWidth() + " x " + (int) image.getHeight());
-        }
-        else {
-          resolutionLabel.setText("");
-        }
-      }
-      else {
-        resolutionLabel.setText("");
-      }
-    } catch (IOException e) {
-      LOG.error("Failed to load raw b2s: " + e.getMessage(), e);
-    }
-  }
-
-  private void refreshMedia(GameMediaRepresentation gameMedia, GameRepresentation game) {
-    GameMediaItemRepresentation item = gameMedia.getItem(PopperScreen.Topper);
-    WidgetFactory.createMediaContainer(screenTopper, client, item);
-
-    item = gameMedia.getItem(PopperScreen.BackGlass);
-    WidgetFactory.createMediaContainer(screenBackglass, client, item);
-
-    item = gameMedia.getItem(PopperScreen.Audio);
-    WidgetFactory.createMediaContainer(screenAudio, client, item);
-
-    item = gameMedia.getItem(PopperScreen.AudioLaunch);
-    WidgetFactory.createMediaContainer(screenAudioLaunch, client, item);
-
-    item = gameMedia.getItem(PopperScreen.DMD);
-    WidgetFactory.createMediaContainer(screenDMD, client, item);
-
-    item = gameMedia.getItem(PopperScreen.GameInfo);
-    WidgetFactory.createMediaContainer(screenInfo, client, item);
-
-    item = gameMedia.getItem(PopperScreen.GameHelp);
-    WidgetFactory.createMediaContainer(screenHelp, client, item);
-
-    item = gameMedia.getItem(PopperScreen.PlayField);
-    WidgetFactory.createMediaContainer(screenPlayfield, client, item);
-
-    item = gameMedia.getItem(PopperScreen.Menu);
-    WidgetFactory.createMediaContainer(screenApron, client, item);
-
-    item = gameMedia.getItem(PopperScreen.Loading);
-    WidgetFactory.createMediaContainer(screenLoading, client, item);
-
-    item = gameMedia.getItem(PopperScreen.Other2);
-    WidgetFactory.createMediaContainer(screenOther2, client, item);
-
-    item = gameMedia.getItem(PopperScreen.Wheel);
-    WidgetFactory.createMediaContainer(screenWheel, client, item);
-  }
-
-  private void disposeMediaPane(BorderPane parent) {
-    if (parent.getCenter() != null) {
-      WidgetFactory.disposeMediaBorderPane(parent);
-    }
-  }
-
   @Override
   public void dispose() {
-    resetMedia();
+
   }
 }
---
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/TablesSidebarController.java
@@ -0,0 +1,431 @@
+package de.mephisto.vpin.ui.tables;
+
+import de.mephisto.vpin.restclient.PopperScreen;
+import de.mephisto.vpin.restclient.VPinStudioClient;
+import de.mephisto.vpin.restclient.representations.GameMediaItemRepresentation;
+import de.mephisto.vpin.restclient.representations.GameMediaRepresentation;
+import de.mephisto.vpin.restclient.representations.GameRepresentation;
+import de.mephisto.vpin.ui.StudioFXController;
+import de.mephisto.vpin.ui.util.BindingUtil;
+import de.mephisto.vpin.ui.util.MediaUtil;
+import de.mephisto.vpin.ui.util.WidgetFactory;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaView;
+import org.apache.commons.lang3.StringUtils;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.*;
+
+public class TablesSidebarController implements Initializable, StudioFXController {
+  private final static Logger LOG = LoggerFactory.getLogger(TablesSidebarController.class);
+
+  @FXML
+  private BorderPane screenTopper;
+
+  @FXML
+  private BorderPane screenBackglass;
+
+  @FXML
+  private BorderPane screenDMD;
+
+  @FXML
+  private BorderPane screenPlayfield;
+
+  @FXML
+  private BorderPane screenApron;
+
+  @FXML
+  private BorderPane screenOther2;
+
+  @FXML
+  private BorderPane screenWheel;
+
+  @FXML
+  private BorderPane screenInfo;
+
+  @FXML
+  private BorderPane screenHelp;
+
+  @FXML
+  private BorderPane screenLoading;
+
+  @FXML
+  private BorderPane screenAudio;
+
+  @FXML
+  private BorderPane screenAudioLaunch;
+
+  @FXML
+  private Button playfieldViewBtn;
+
+  @FXML
+  private Accordion accordion;
+
+  @FXML
+  private TitledPane titledPaneMedia;
+
+  @FXML
+  private Label labelId;
+
+  @FXML
+  private Label labelRom;
+
+  @FXML
+  private Label labelRomAlias;
+
+  @FXML
+  private Label labelNVOffset;
+
+  @FXML
+  private Label labelFilename;
+
+  @FXML
+  private Label labelLastPlayed;
+
+  @FXML
+  private Label labelTimesPlayed;
+
+  @FXML
+  private Label labelHSFilename;
+
+  @FXML
+  private Slider volumeSlider;
+
+  @FXML
+  private TextArea highscoreTextArea;
+
+  @FXML
+  private ImageView rawDirectB2SImage;
+
+  @FXML
+  private Button openDirectB2SImageButton;
+
+  @FXML
+  private Button editHsFileNameBtn;
+
+  @FXML
+  private Button editRomNameBtn;
+
+  @FXML
+  private Label resolutionLabel;
+
+
+  private VPinStudioClient client;
+
+  private Optional<GameRepresentation> game = Optional.empty();
+
+  private TablesController tablesController;
+
+  // Add a public no-args constructor
+  public TablesSidebarController() {
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    client = new VPinStudioClient();
+    this.accordion.setExpandedPane(titledPaneMedia);
+
+    volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+      @Override
+      public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+        if (game.isPresent()) {
+          BindingUtil.debouncer.debounce("tableVolume" + game.get().getId(), () -> {
+            int value = t1.intValue();
+            if (value == 0) {
+              value = 1;
+            }
+            game.get().setVolume(value);
+            client.saveGame(game.get());
+          }, 1000);
+        }
+      }
+    });
+
+    titledPaneMedia.expandedProperty().addListener(new ChangeListener<Boolean>() {
+      @Override
+      public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean expanded) {
+        if (expanded) {
+          refreshView(game);
+        } else {
+          resetMedia();
+        }
+      }
+    });
+  }
+
+  public void setTablesController(TablesController tablesController) {
+    this.tablesController = tablesController;
+  }
+
+  public void setGame(Optional<GameRepresentation> game) {
+    this.game = game;
+    this.refreshView(game);
+  }
+
+  @FXML
+  private void onPlayClick(ActionEvent e) {
+    Button source = (Button) e.getSource();
+    BorderPane borderPane = (BorderPane) source.getParent();
+    MediaView mediaView = (MediaView) borderPane.getCenter();
+
+    FontIcon icon = (FontIcon) source.getChildrenUnmodifiable().get(0);
+    String iconLiteral = icon.getIconLiteral();
+    if (iconLiteral.equals("bi-play")) {
+      mediaView.getMediaPlayer().setMute(false);
+      mediaView.getMediaPlayer().setCycleCount(1);
+      mediaView.getMediaPlayer().play();
+      icon.setIconLiteral("bi-stop");
+    } else {
+      mediaView.getMediaPlayer().stop();
+      icon.setIconLiteral("bi-play");
+    }
+  }
+
+  @FXML
+  private void onOpenDirectB2SBackground() {
+    MediaUtil.openDirectB2SBackground(game.get());
+  }
+
+  @FXML
+  private void onMediaViewClick(ActionEvent e) {
+    Button source = (Button) e.getSource();
+    BorderPane borderPane = (BorderPane) source.getParent();
+    Node center = borderPane.getCenter();
+    if (center == null) {
+      center = screenPlayfield.getCenter();
+    }
+
+
+    if (center instanceof MediaView) {
+      MediaView mediaView = (MediaView) center;
+      Media media = mediaView.getMediaPlayer().getMedia();
+      String s = media.getSource();
+      try {
+        Desktop.getDesktop().browse(URI.create(s));
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    } else if (center instanceof ImageView) {
+      ImageView imageView = (ImageView) center;
+      String url = (String) imageView.getUserData();
+      try {
+        Desktop.getDesktop().browse(URI.create(url));
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+
+  @FXML
+  private void onRomEdit() {
+    GameRepresentation gameRepresentation = game.get();
+    String romName = WidgetFactory.showInputDialog("Enter ROM Name", null, gameRepresentation.getRom());
+    if (romName != null) {
+      gameRepresentation.setRom(romName);
+      client.saveGame(gameRepresentation);
+      tablesController.onReload();
+    }
+  }
+
+  @FXML
+  private void onDismiss() {
+    GameRepresentation gameRepresentation = game.get();
+    Optional<ButtonType> result = WidgetFactory.showConfirmation("Ignore this warning for future validations of table '" + gameRepresentation.getGameDisplayName() + "?", null);
+    if (result.isPresent() && result.get().equals(ButtonType.OK)) {
+      String validationState = String.valueOf(gameRepresentation.getValidationState());
+      String ignoredValidations = gameRepresentation.getIgnoredValidations();
+      if (ignoredValidations == null) {
+        ignoredValidations = "";
+      }
+      List<String> gameIgnoreList = new ArrayList<>(Arrays.asList(ignoredValidations.split(",")));
+      if (!gameIgnoreList.contains(validationState)) {
+        gameIgnoreList.add(validationState);
+      }
+
+      gameRepresentation.setIgnoredValidations(StringUtils.join(gameIgnoreList, ","));
+      client.saveGame(gameRepresentation);
+      tablesController.onReload();
+    }
+  }
+
+  @FXML
+  private void onHsFileNameEdit() {
+    GameRepresentation gameRepresentation = game.get();
+    String fs = WidgetFactory.showInputDialog("EM Highscore Filename", "Enter the name of the highscore file for this table.\nThe file is located in the 'User' folder.", gameRepresentation.getHsFileName());
+    if (fs != null) {
+      gameRepresentation.setHsFileName(fs);
+      client.saveGame(gameRepresentation);
+      tablesController.onReload();
+    }
+  }
+
+  private void resetMedia() {
+    disposeMediaPane(screenAudioLaunch);
+    disposeMediaPane(screenAudio);
+    disposeMediaPane(screenLoading);
+    disposeMediaPane(screenHelp);
+    disposeMediaPane(screenInfo);
+    disposeMediaPane(screenDMD);
+    disposeMediaPane(screenBackglass);
+    disposeMediaPane(screenTopper);
+    disposeMediaPane(screenApron);
+    disposeMediaPane(screenPlayfield);
+    disposeMediaPane(screenOther2);
+    disposeMediaPane(screenWheel);
+  }
+
+  private void refreshView(Optional<GameRepresentation> g) {
+    editHsFileNameBtn.setDisable(g.isEmpty());
+    editRomNameBtn.setDisable(g.isEmpty());
+
+    if (g.isPresent()) {
+      GameRepresentation game = g.get();
+      GameMediaRepresentation gameMedia = client.getGameMedia(game.getId());
+
+      volumeSlider.setDisable(false);
+      volumeSlider.setValue(game.getVolume());
+
+      GameMediaItemRepresentation item = gameMedia.getItem(PopperScreen.PlayField);
+      playfieldViewBtn.setVisible(item != null);
+
+      labelId.setText(String.valueOf(game.getId()));
+      labelRom.setText(game.getOriginalRom() != null ? game.getOriginalRom() : game.getRom());
+      labelRomAlias.setText(game.getOriginalRom() != null ? game.getRom() : "-");
+      labelNVOffset.setText(game.getNvOffset() > 0 ? String.valueOf(game.getNvOffset()) : "-");
+      labelFilename.setText(game.getGameFileName());
+      labelLastPlayed.setText(game.getLastPlayed() != null ? game.getLastPlayed().toString() : "-");
+      labelTimesPlayed.setText(String.valueOf(game.getNumberPlays()));
+      labelHSFilename.setText(game.getHsFileName());
+
+      refreshDirectB2SPreview(g);
+
+      if (titledPaneMedia.isExpanded()) {
+        refreshMedia(gameMedia, game);
+      }
+
+      String rawHighscore = game.getRawHighscore();
+      if (rawHighscore != null) {
+        highscoreTextArea.setText(rawHighscore);
+      } else {
+        highscoreTextArea.setText("");
+      }
+    } else {
+      resetMedia();
+
+      playfieldViewBtn.setVisible(false);
+
+      volumeSlider.setValue(100);
+      volumeSlider.setDisable(true);
+
+      labelId.setText("-");
+      labelRom.setText("-");
+      labelRomAlias.setText("-");
+      labelNVOffset.setText("-");
+      labelFilename.setText("-");
+      labelLastPlayed.setText("-");
+      labelTimesPlayed.setText("-");
+      labelHSFilename.setText("-");
+
+      refreshDirectB2SPreview(Optional.empty());
+
+      highscoreTextArea.setText("");
+    }
+  }
+
+  private void refreshDirectB2SPreview(Optional<GameRepresentation> game) {
+    try {
+      openDirectB2SImageButton.setVisible(false);
+      openDirectB2SImageButton.setTooltip(new Tooltip("Open directb2s image"));
+      rawDirectB2SImage.setVisible(false);
+
+      if (game.isPresent()) {
+        InputStream input = client.getDirectB2SImage(game.get());
+        Image image = new Image(input);
+        rawDirectB2SImage.setVisible(true);
+        rawDirectB2SImage.setImage(image);
+        input.close();
+
+        if (image.getWidth() > 300) {
+          openDirectB2SImageButton.setVisible(true);
+          resolutionLabel.setText("Resolution: " + (int) image.getWidth() + " x " + (int) image.getHeight());
+        } else {
+          resolutionLabel.setText("");
+        }
+      } else {
+        resolutionLabel.setText("");
+      }
+    } catch (IOException e) {
+      LOG.error("Failed to load raw b2s: " + e.getMessage(), e);
+    }
+  }
+
+  private void refreshMedia(GameMediaRepresentation gameMedia, GameRepresentation game) {
+    GameMediaItemRepresentation item = gameMedia.getItem(PopperScreen.Topper);
+    WidgetFactory.createMediaContainer(screenTopper, client, item);
+
+    item = gameMedia.getItem(PopperScreen.BackGlass);
+    WidgetFactory.createMediaContainer(screenBackglass, client, item);
+
+    item = gameMedia.getItem(PopperScreen.Audio);
+    WidgetFactory.createMediaContainer(screenAudio, client, item);
+
+    item = gameMedia.getItem(PopperScreen.AudioLaunch);
+    WidgetFactory.createMediaContainer(screenAudioLaunch, client, item);
+
+    item = gameMedia.getItem(PopperScreen.DMD);
+    WidgetFactory.createMediaContainer(screenDMD, client, item);
+
+    item = gameMedia.getItem(PopperScreen.GameInfo);
+    WidgetFactory.createMediaContainer(screenInfo, client, item);
+
+    item = gameMedia.getItem(PopperScreen.GameHelp);
+    WidgetFactory.createMediaContainer(screenHelp, client, item);
+
+    item = gameMedia.getItem(PopperScreen.PlayField);
+    WidgetFactory.createMediaContainer(screenPlayfield, client, item);
+
+    item = gameMedia.getItem(PopperScreen.Menu);
+    WidgetFactory.createMediaContainer(screenApron, client, item);
+
+    item = gameMedia.getItem(PopperScreen.Loading);
+    WidgetFactory.createMediaContainer(screenLoading, client, item);
+
+    item = gameMedia.getItem(PopperScreen.Other2);
+    WidgetFactory.createMediaContainer(screenOther2, client, item);
+
+    item = gameMedia.getItem(PopperScreen.Wheel);
+    WidgetFactory.createMediaContainer(screenWheel, client, item);
+  }
+
+  private void disposeMediaPane(BorderPane parent) {
+    if (parent.getCenter() != null) {
+      WidgetFactory.disposeMediaBorderPane(parent);
+    }
+  }
+
+  @Override
+  public void dispose() {
+    resetMedia();
+  }
+}
