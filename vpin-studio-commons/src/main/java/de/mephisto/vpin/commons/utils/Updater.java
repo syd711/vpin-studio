e00b3937ce97cf2b4933f53fe378125dd619d0b5
Matthias Faust <syd711@gmail.com>
2023-03-28 20:53:48 +0200

    tweaks

--- vpin-studio-commons/src/main/java/de/mephisto/vpin/commons/utils/Updater.java
+++ vpin-studio-commons/src/main/java/de/mephisto/vpin/commons/utils/Updater.java
@@ -84,7 +84,7 @@ public class Updater {
   }
 
   public static boolean installServerUpdate() throws IOException {
-    FileUtils.writeBatch("update-server.bat", "timeout /T 4 /nobreak\nresources\\7z.exe -aoa x \"VPin-Studio-Server.zip\"\ndel VPin-Studio-Server.zip\nserver.vbs\nexit");
+    FileUtils.writeBatch("update-server.bat", "timeout /T 4 /nobreak\nresources\\7z.exe -aoa x \"VPin-Studio-Server.zip\"\ntimeout /T 4 /nobreak\ndel VPin-Studio-Server.zip\nserver.vbs\nexit");
     List<String> commands = Arrays.asList("cmd", "/c", "start", "update-server.bat");
     SystemCommandExecutor executor = new SystemCommandExecutor(commands);
     executor.setDir(getBasePath());
@@ -102,7 +102,7 @@ public class Updater {
   }
 
   public static boolean installClientUpdate() throws IOException {
-    String cmds = "timeout /T 4 /nobreak\nresources\\7z.exe -aoa x \"VPin-Studio.zip\"\ndel VPin-Studio.zip\nVPin-Studio.exe\nexit";
+    String cmds = "timeout /T 4 /nobreak\nresources\\7z.exe -aoa x \"VPin-Studio.zip\"\ntimeout /T 4 /nobreak\ndel VPin-Studio.zip\nVPin-Studio.exe\nexit";
     FileUtils.writeBatch("update-client.bat", cmds);
     LOG.info("Written temporary batch: " + cmds);
     List<String> commands = Arrays.asList("cmd", "/c", "start", "update-client.bat");
