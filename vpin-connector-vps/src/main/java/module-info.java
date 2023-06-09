381c5387c5bf8947b0f30143bd9faf091a21b05b
Matthias Faust <syd711@gmail.com>
2023-06-09 18:13:20 +0200

    VPS

---
+++ vpin-connector-vps/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+open module de.mephisto.vpin.connectors.vps {
+  requires org.slf4j;
+  requires java.desktop;
+  requires com.fasterxml.jackson.databind;
+  exports de.mephisto.vpin.connectors.vps;
+}
