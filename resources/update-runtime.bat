8d43d51a85b1a7bb12b8c7ceda1cba9744e4d004
Matthias Faust <syd711@gmail.com>
2023-12-10 07:55:45 +0100

    runtime update

--- resources/update-runtime.bat
+++ resources/update-runtime.bat
@@ -1,19 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
+if exist "java-runtime.zip" (
+	del "java-runtime.zip"
+	echo Deleted existing runtime archive.
+)
+
 REM Set the URL of the zip file you want to download
-set "zipUrl=https://example.com/path/to/your/file.zip"
+set "zipUrl=https://github.com/syd711/vpin-studio/blob/main/java-runtime.zip?raw=true"
 
 REM Set the path where you want to download and extract the zip file
-set "downloadPath=C:\Path\To\Download"
-set "extractPath=C:\Path\To\Extract"
+set "downloadPath=.\"
+set "extractPath=.\..\win32\java\"
 
 REM Create the directories if they don't exist
 if not exist "!downloadPath!" mkdir "!downloadPath!"
 if not exist "!extractPath!" mkdir "!extractPath!"
 
 REM Download the zip file using PowerShell
-powershell -Command "& { Invoke-WebRequest -Uri '%zipUrl%' -OutFile '!downloadPath!\file.zip' }"
+powershell -Command "& { Invoke-WebRequest -Uri '%zipUrl%' -OutFile '!downloadPath!\java-runtime.zip' }"
 
 REM Check if the download was successful
 if %errorlevel% neq 0 (
@@ -21,9 +26,20 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-REM Extract the contents of the zip file
-powershell -Command "& { Expand-Archive -Path '!downloadPath!\file.zip' -DestinationPath '!extractPath!' }"
+echo Download completed.
 
-echo Extraction complete.
+if exist "%extractPath%" (
+    pushd "%extractPath%"
+    del /q *.*
+    popd
+    echo All files in %extractPath% have been deleted.
+) else (
+    echo Folder not found: %extractPath%
+)
+
+REM Extract the contents of the zip file
+powershell -Command "& { Expand-Archive -force -Path '!downloadPath!java-runtime.zip' -DestinationPath '!extractPath!' }"
+
+echo Runtime extraction completed, runtime update finished.
 
 endlocal
