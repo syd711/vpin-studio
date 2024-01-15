261ca39dae5e226e37d1a0ea0d308517ad61cabd
syd711 <syd711@gmail.com>
2024-01-15 14:24:04 +0100

    facts

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/SplashScreenController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/SplashScreenController.java
@@ -3,9 +3,13 @@ package de.mephisto.vpin.ui;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SplashScreenController implements Initializable {
 
@@ -15,5 +19,13 @@ public class SplashScreenController implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     versionLabel.setText("Version " + Studio.getVersion());
+
+    try {
+      List<String> facts = IOUtils.readLines(SplashScreenController.class.getResourceAsStream("facts.txt"));
+      int i = ThreadLocalRandom.current().nextInt(0, facts.size() - 1);
+      String fact = facts.get(i);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
