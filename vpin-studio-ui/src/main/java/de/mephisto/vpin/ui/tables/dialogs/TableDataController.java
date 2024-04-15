43edd304de8272c79dc6845222896a1a5e63492d
Matthias Faust <syd711@gmail.com>
2024-04-15 19:49:43 +0200

    separated screens tab

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/dialogs/TableDataController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/dialogs/TableDataController.java
@@ -216,45 +216,8 @@ public class TableDataController implements Initializable, DialogController, Aut
   @FXML
   private Slider volumeSlider;
 
-  //screens
   @FXML
-  private CheckBox useEmuDefaultsCheckbox;
-
-  @FXML
-  private CheckBox hideAllCheckbox;
-
-  @FXML
-  private CheckBox topperCheckbox;
-
-  @FXML
-  private CheckBox dmdCheckbox;
-
-  @FXML
-  private CheckBox backglassCheckbox;
-
-  @FXML
-  private CheckBox playfieldCheckbox;
-
-  @FXML
-  private CheckBox musicCheckbox;
-
-  @FXML
-  private CheckBox apronCheckbox;
-
-  @FXML
-  private CheckBox wheelbarCheckbox;
-
-  @FXML
-  private CheckBox loadingCheckbox;
-
-  @FXML
-  private CheckBox otherCheckbox;
-
-  @FXML
-  private CheckBox flyerCheckbox;
-
-  @FXML
-  private CheckBox helpCheckbox;
+  private Tab screensTab;
 
   @FXML
   private ComboBox<TableStatus> statusCombo;
@@ -331,7 +294,6 @@ public class TableDataController implements Initializable, DialogController, Aut
   private ComboBox<VpsTableVersion> tableVersionsCombo;
   private AutoCompleteTextField autoCompleteNameField;
 
-  private List<CheckBox> screenCheckboxes = new ArrayList<>();
   private TableOverviewController overviewController;
 
   private GameRepresentation game;
@@ -348,6 +310,7 @@ public class TableDataController implements Initializable, DialogController, Aut
   private ScoringDB scoringDB;
   private HighscoreFiles highscoreFiles;
   private TableDataTabStatisticsController tableStatisticsController;
+  private TableDataTabScreensController tableScreensController;
 
   @FXML
   private void onAssetManager(ActionEvent e) {
@@ -650,31 +613,7 @@ public class TableDataController implements Initializable, DialogController, Aut
       }
     }
 
-
-    String value = "";
-    if (useEmuDefaultsCheckbox.isSelected()) {
-      //nothing, empty value for defaults
-    }
-    else if (hideAllCheckbox.isSelected()) {
-      value = "NONE";
-    }
-    else {
-      List<String> result = new ArrayList<>();
-      if (topperCheckbox.isSelected()) result.add("" + 0);
-      if (dmdCheckbox.isSelected()) result.add("" + 1);
-      if (backglassCheckbox.isSelected()) result.add("" + 2);
-      if (playfieldCheckbox.isSelected()) result.add("" + 3);
-      if (musicCheckbox.isSelected()) result.add("" + 4);
-      if (apronCheckbox.isSelected()) result.add("" + 5);
-      if (wheelbarCheckbox.isSelected()) result.add("" + 6);
-      if (loadingCheckbox.isSelected()) result.add("" + 7);
-      if (otherCheckbox.isSelected()) result.add("" + 8);
-      if (flyerCheckbox.isSelected()) result.add("" + 9);
-      if (helpCheckbox.isSelected()) result.add("" + 10);
-
-      value = String.join(",", result);
-    }
-    tableDetails.setKeepDisplays(value);
+    tableScreensController.save();
 
     try {
       if (closeDialog) {
@@ -710,96 +649,20 @@ public class TableDataController implements Initializable, DialogController, Aut
 
     openHsFileBtn.setVisible(client.getSystemService().isLocal());
 
-    //screens
-    screenCheckboxes = Arrays.asList(topperCheckbox, dmdCheckbox, backglassCheckbox, playfieldCheckbox, musicCheckbox,
-      apronCheckbox, wheelbarCheckbox, loadingCheckbox, otherCheckbox, flyerCheckbox, helpCheckbox);
-
-    useEmuDefaultsCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        screenCheckboxes.stream().forEach(check -> check.setSelected(false));
-        hideAllCheckbox.setSelected(false);
-      }
-    });
-
-    hideAllCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        screenCheckboxes.stream().forEach(check -> check.setSelected(false));
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-
-    topperCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    dmdCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    backglassCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    playfieldCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    musicCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    apronCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    wheelbarCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    loadingCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    otherCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    flyerCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-    helpCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        hideAllCheckbox.setSelected(false);
-        useEmuDefaultsCheckbox.setSelected(false);
-      }
-    });
-
     try {
       FXMLLoader loader = new FXMLLoader(TableDataTabStatisticsController.class.getResource("dialog-table-data-tab-statistics.fxml"));
       Parent builtInRoot = loader.load();
       tableStatisticsController = loader.getController();
       statisticsTab.setContent(builtInRoot);
+    } catch (IOException e) {
+      LOG.error("Failed to load tile: " + e.getMessage(), e);
+    }
+
+    try {
+      FXMLLoader loader = new FXMLLoader(TableDataTabScreensController.class.getResource("dialog-table-data-tab-screens.fxml"));
+      Parent builtInRoot = loader.load();
+      tableScreensController = loader.getController();
+      screensTab.setContent(builtInRoot);
     } catch (IOException e) {
       LOG.error("Failed to load tile: " + e.getMessage(), e);
     }
@@ -1040,71 +903,6 @@ public class TableDataController implements Initializable, DialogController, Aut
       this.tableDetails.setAltLaunchExe(t1);
     });
 
-    //displays
-    String keepDisplays = tableDetails.getKeepDisplays();
-    if (StringUtils.isEmpty(keepDisplays)) {
-      useEmuDefaultsCheckbox.setSelected(true);
-    }
-    else if (keepDisplays.equalsIgnoreCase("NONE")) {
-      hideAllCheckbox.setSelected(true);
-    }
-    else {
-      String[] split = keepDisplays.split(",");
-      for (String screen : split) {
-        if (StringUtils.isEmpty(screen)) {
-          continue;
-        }
-
-        int id = Integer.parseInt(screen);
-        switch (id) {
-          case 0: {
-            topperCheckbox.setSelected(true);
-            break;
-          }
-          case 1: {
-            dmdCheckbox.setSelected(true);
-            break;
-          }
-          case 2: {
-            backglassCheckbox.setSelected(true);
-            break;
-          }
-          case 3: {
-            playfieldCheckbox.setSelected(true);
-            break;
-          }
-          case 4: {
-            musicCheckbox.setSelected(true);
-            break;
-          }
-          case 5: {
-            apronCheckbox.setSelected(true);
-            break;
-          }
-          case 6: {
-            wheelbarCheckbox.setSelected(true);
-            break;
-          }
-          case 7: {
-            loadingCheckbox.setSelected(true);
-            break;
-          }
-          case 8: {
-            otherCheckbox.setSelected(true);
-            break;
-          }
-          case 9: {
-            flyerCheckbox.setSelected(true);
-            break;
-          }
-          case 10: {
-            helpCheckbox.setSelected(true);
-            break;
-          }
-        }
-      }
-    }
-
     if (tableDetails.getVolume() != null) {
       try {
         volumeSlider.setValue(Integer.parseInt(tableDetails.getVolume()));
@@ -1195,6 +993,7 @@ public class TableDataController implements Initializable, DialogController, Aut
 
     initVpsStatus();
     tableStatisticsController.setGame(game, tableDetails);
+    tableScreensController.setGame(game, tableDetails);
     tabPane.getSelectionModel().select(tab);
   }
 
