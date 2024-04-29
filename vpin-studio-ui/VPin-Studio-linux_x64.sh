986b6d3135e9063e39c4590a168b050c86d08a71
Matthias Faust <syd711@gmail.com>
2024-04-29 18:57:48 +0200

    new osx packing

---
+++ vpin-studio-ui/VPin-Studio-linux_x64.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+tar -xvf zulu11.72.19-ca-fx-jre11.0.23-linux_x64.tar.gz
+./zulu11.72.19-ca-fx-jre11.0.23-linux_x64/bin/java -jar vpin-studio-ui.jar
---
+++ vpin-studio-ui/VPin-Studio-macosx_aarch64.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+tar -xvf zulu11.72.19-ca-fx-jre11.0.23-macosx_aarch64.tar.gz
+./zulu11.72.19-ca-fx-jre11.0.23-macosx_aarch64/zulu-11.jre/Contents/Home/bin/java -jar vpin-studio-ui.jar
