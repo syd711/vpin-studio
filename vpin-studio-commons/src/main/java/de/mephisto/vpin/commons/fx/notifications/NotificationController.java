f6cf7bca57f0414f4f915c505b09f9cc869fb05d
Matthias Faust <syd711@gmail.com>
2024-05-06 15:03:32 +0200

    Theming

--- vpin-studio-commons/src/main/java/de/mephisto/vpin/commons/fx/notifications/NotificationController.java
+++ vpin-studio-commons/src/main/java/de/mephisto/vpin/commons/fx/notifications/NotificationController.java
@@ -3,12 +3,15 @@ package de.mephisto.vpin.commons.fx.notifications;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,17 +34,40 @@ public class NotificationController implements Initializable {
   private ImageView rowImageView;
 
   @FXML
+  private ImageView rowIconImageView;
+
+  @FXML
   private VBox labelContainer;
 
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    try {
+      File themeFolder = new File("./resources/themes/notifications");
+      if (themeFolder.exists()) {
+        File row = new File(themeFolder, "row.png");
+        if (row.exists()) {
+          Image image = new Image(new FileInputStream(row));
+          rowImageView.setImage(image);
+        }
 
+        File rowImage = new File(themeFolder, "logo.png");
+        if (rowImage.exists()) {
+          Image image = new Image(new FileInputStream(rowImage));
+          rowIconImageView.setImage(image);
+        }
+      }
+    } catch (Exception e) {
+      LOG.error("Failed to theme notifications: " + e.getMessage(), e);
+    }
   }
 
   public void setNotification(Notification notification) {
     if (notification.getImage() != null) {
       logoImageView.setImage(notification.getImage());
+    }
+    else {
+      logoImageView.setVisible(false);
     }
 
     if (notification.getTitle1() != null) {
@@ -54,7 +80,7 @@ public class NotificationController implements Initializable {
     if (notification.getTitle2() != null) {
       title2.setText(notification.getTitle2());
     }
-    else{
+    else {
       title2.setText("");
     }
 
--- vpin-studio-server/src/main/java/de/mephisto/vpin/server/notifications/NotificationService.java
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/notifications/NotificationService.java
@@ -1,50 +1,105 @@
 package de.mephisto.vpin.server.notifications;
 
+import de.mephisto.vpin.commons.fx.Features;
 import de.mephisto.vpin.commons.fx.notifications.Notification;
 import de.mephisto.vpin.commons.fx.notifications.NotificationStage;
 import de.mephisto.vpin.commons.fx.notifications.NotificationStageService;
 import de.mephisto.vpin.restclient.PreferenceNames;
 import de.mephisto.vpin.restclient.notifications.NotificationSettings;
+import de.mephisto.vpin.restclient.popper.PopperScreen;
+import de.mephisto.vpin.server.games.Game;
+import de.mephisto.vpin.server.highscores.Highscore;
+import de.mephisto.vpin.server.highscores.HighscoreChangeEvent;
+import de.mephisto.vpin.server.highscores.HighscoreChangeListener;
+import de.mephisto.vpin.server.highscores.HighscoreService;
+import de.mephisto.vpin.server.popper.GameMediaItem;
 import de.mephisto.vpin.server.preferences.PreferenceChangedListener;
 import de.mephisto.vpin.server.preferences.PreferencesService;
+import de.mephisto.vpin.server.util.ImageUtil;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 @Service
-public class NotificationService implements InitializingBean, PreferenceChangedListener {
+public class NotificationService implements InitializingBean, PreferenceChangedListener, HighscoreChangeListener {
   private final static Logger LOG = LoggerFactory.getLogger(NotificationService.class);
 
   @Autowired
   private PreferencesService preferencesService;
 
+  @Autowired
+  private HighscoreService highscoreService;
+
   private NotificationSettings notificationSettings;
 
   public void showNotification(Notification notification) {
-    notification.setDurationSec(notification.getDurationSec());
-    NotificationStageService.getInstance().showNotification(notification);
+    if (Features.NOTIFICATIONS_ENABLED) {
+      notification.setDurationSec(notification.getDurationSec());
+      NotificationStageService.getInstance().showNotification(notification);
+    }
   }
 
   @Override
   public void preferenceChanged(String propertyName, Object oldValue, Object newValue) throws Exception {
     if (propertyName.equals(PreferenceNames.NOTIFICATION_SETTINGS)) {
       notificationSettings = preferencesService.getJsonPreference(PreferenceNames.NOTIFICATION_SETTINGS, NotificationSettings.class);
-      LOG.info("Notification settings have been updated.");
     }
   }
 
   @Override
-  public void afterPropertiesSet() throws Exception {
-    preferenceChanged(PreferenceNames.NOTIFICATION_SETTINGS, null, null);
+  public void highscoreChanged(@NotNull HighscoreChangeEvent event) {
+    if (!notificationSettings.isHighscoreUpdatedNotification() || notificationSettings.getDurationSec() == 0) {
+      return;
+    }
 
-    Notification startup = new Notification();
-    startup.setImage(new Image(NotificationStage.class.getResourceAsStream("logo.png")));
-    startup.setTitle1("VPin Studio Server");
-    startup.setTitle2("The server has been started.");
-    startup.setDurationSec(notificationSettings.getDurationSec());
-    showNotification(startup);
+    Game game = event.getGame();
+    GameMediaItem gameMediaItem = game.getGameMedia().getDefaultMediaItem(PopperScreen.Wheel);
+    Image image = null;
+    if (gameMediaItem != null) {
+      try {
+        BufferedImage bufferedImage = ImageUtil.loadImage(gameMediaItem.getFile());
+        image = SwingFXUtils.toFXImage(bufferedImage, null);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    Notification notification = new Notification();
+    notification.setImage(image);
+    notification.setDurationSec(notificationSettings.getDurationSec());
+    notification.setTitle1(game.getGameDisplayName());
+    notification.setTitle2("A new highscore has been created!");
+    notification.setTitle3(event.getNewScore().getPosition() + ". " + event.getNewScore().getPlayerInitials() + "\t" + event.getNewScore().getScore());
+    showNotification(notification);
+  }
+
+  @Override
+  public void highscoreUpdated(@NotNull Game game, @NotNull Highscore highscore) {
+
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    if (Features.NOTIFICATIONS_ENABLED) {
+      preferencesService.addChangeListener(this);
+      highscoreService.addHighscoreChangeListener(this);
+      preferenceChanged(PreferenceNames.NOTIFICATION_SETTINGS, null, null);
+
+      if (notificationSettings.isStartupNotification() && notificationSettings.getDurationSec() > 0) {
+        Notification startup = new Notification();
+        startup.setImage(null);
+        startup.setTitle1("VPin Studio Server");
+        startup.setTitle2("The server has been started.");
+        startup.setDurationSec(notificationSettings.getDurationSec());
+        showNotification(startup);
+      }
+    }
   }
 }
