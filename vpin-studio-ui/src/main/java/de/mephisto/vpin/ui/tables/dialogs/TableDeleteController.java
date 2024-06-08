005c1ebf73b97afdffd8f542f99bda8171e3c7c1
Matthias Faust <syd711@gmail.com>
2024-06-08 13:05:43 +0200

    better preselection

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/dialogs/TableDeleteController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/dialogs/TableDeleteController.java
@@ -129,6 +129,12 @@ public class TableDeleteController implements Initializable, DialogController {
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     this.deleteBtn.setDisable(true);
+    vpxFileCheckbox.setSelected(true);
+    iniCheckbox.setSelected(true);
+    directb2sCheckbox.setSelected(true);
+    vbsCheckbox.setSelected(true);
+    povCheckbox.setSelected(true);
+    resCheckbox.setSelected(true);
     confirmationCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> deleteBtn.setDisable(!newValue));
 
     deleteAllCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
