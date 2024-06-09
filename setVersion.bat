3bcbeeadff458b10085726f7319b274b4116fb15
Matthias Faust <syd711@gmail.com>
2024-06-09 11:30:37 +0200

    alt sound cache

--- setVersion.bat
+++ setVersion.bat
@@ -1,12 +1,1 @@
-mvn versions:set -DnewVersion="%1" -DgenerateBackupPoms=false -DprocessAllModules
-cd vpin-studio-server
-mvn versions:set -DnewVersion="%1" -DgenerateBackupPoms=false -DprocessAllModules
-cd ..
-cd vpin-studio-ui
-mvn versions:set -DnewVersion="%1" -DgenerateBackupPoms=false -DprocessAllModules
-cd ..
-cd vps-bot
-mvn versions:set -DnewVersion="%1" -DgenerateBackupPoms=false -DprocessAllModules
-cd ..
-mvn license:aggregate-download-licenses
-generateLicenses.bat
+mvn versions:set -DnewVersion="%1" -DgenerateBackupPoms=false
