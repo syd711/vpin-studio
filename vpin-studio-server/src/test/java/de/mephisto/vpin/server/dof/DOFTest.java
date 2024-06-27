58875dae7701c1acf794b829e04607c6d99c4ccf
leprinco <leprinco@yahoo.fr>
2024-06-27 08:31:20 +0200

    fix more tests

--- vpin-studio-server/src/test/java/de/mephisto/vpin/server/dof/DOFTest.java
+++ vpin-studio-server/src/test/java/de/mephisto/vpin/server/dof/DOFTest.java
@@ -4,13 +4,12 @@ import de.mephisto.vpin.server.util.WindowsShortcut;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 
 public class DOFTest {
 
   @Test
   public void testDof() throws Exception {
-    File dofConfigFolder = new File("C:\\vPinball\\visualpinball\\Tables\\Plugins64", "DirectOutputx64.lnk");
+    File dofConfigFolder = new File("../testsystem/vPinball/visualpinball/Tables/Plugins64", "DirectOutputx64.lnk");
     boolean potentialValidLink = WindowsShortcut.isPotentialValidLink(dofConfigFolder);
     System.out.println(potentialValidLink);
 
