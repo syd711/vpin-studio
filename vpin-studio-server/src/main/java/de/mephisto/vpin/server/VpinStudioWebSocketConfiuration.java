b74ebd965e0f4c9bac6c0956e75827d6c1bcc7db
leprinco <leprinco@yahoo.fr>
2024-07-29 21:20:28 +0200

    first version for  DMDScore processor

---
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/VpinStudioWebSocketConfiuration.java
@@ -0,0 +1,19 @@
+package de.mephisto.vpin.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import de.mephisto.vpin.server.score.DMDScoreWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class VpinStudioWebSocketConfiuration implements WebSocketConfigurer {
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(new DMDScoreWebSocketHandler(), "/dmdsocket").setAllowedOrigins("*");
+  }
+
+}
