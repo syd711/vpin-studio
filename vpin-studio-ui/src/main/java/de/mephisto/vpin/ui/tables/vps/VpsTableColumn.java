2791dc22c802e5866539ca4c16ab4a8fbd911fb2
Matthias Faust <syd711@gmail.com>
2024-08-05 18:40:27 +0200

    3.0.7

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/vps/VpsTableColumn.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tables/vps/VpsTableColumn.java
@@ -2,7 +2,6 @@ package de.mephisto.vpin.ui.tables.vps;
 
 import de.mephisto.vpin.commons.utils.WidgetFactory;
 import de.mephisto.vpin.connectors.vps.model.*;
-import de.mephisto.vpin.restclient.games.GameRepresentation;
 import de.mephisto.vpin.restclient.preferences.UISettings;
 import de.mephisto.vpin.ui.Studio;
 import edu.umd.cs.findbugs.annotations.Nullable;
@@ -11,94 +10,103 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import org.kordamp.ikonli.javafx.FontIcon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class VpsTableColumn extends HBox {
+  private final static Logger LOG = LoggerFactory.getLogger(VpsTableColumn.class);
 
   public VpsTableColumn(@Nullable String vpsTableId, @Nullable String vpsTableVersionId, @Nullable VPSChanges updates, UISettings uiSettings) {
     super(3);
-    int iconSize = 14;
+    try {
+      int iconSize = 14;
 
-    this.setAlignment(Pos.CENTER_LEFT);
+      this.setAlignment(Pos.CENTER_LEFT);
 
-    Label label = new Label();
-    label.getStyleClass().add("default-title");
-    VpsTable vpsTable = Studio.client.getVpsService().getTableById(vpsTableId);
-    VpsTableVersion vpsTableVersion = null;
+      Label label = new Label();
+      label.getStyleClass().add("default-title");
+      VpsTable vpsTable = Studio.client.getVpsService().getTableById(vpsTableId);
+      VpsTableVersion vpsTableVersion = null;
 
-    if (vpsTable != null) {
-      vpsTableVersion = vpsTable.getTableVersionById(vpsTableVersionId);
+      if (vpsTable != null) {
+        vpsTableVersion = vpsTable.getTableVersionById(vpsTableVersionId);
 
-      FontIcon checkboxIcon = WidgetFactory.createCheckboxIcon();
-      checkboxIcon.setIconSize(iconSize);
-      label.setGraphic(checkboxIcon);
-      label.setTooltip(new Tooltip("VPS Table:\n" + vpsTable.getDisplayName()));
-    }
-    else {
-      label.setText(" - ");
+        FontIcon checkboxIcon = WidgetFactory.createCheckboxIcon();
+        checkboxIcon.setIconSize(iconSize);
+        label.setGraphic(checkboxIcon);
+        label.setTooltip(new Tooltip("VPS Table:\n" + vpsTable.getDisplayName()));
+      }
+      else {
+        label.setText(" - ");
+        label.setStyle("-fx-text-fill: #FFFFFF;");
+        label.setTooltip(new Tooltip("No VPS table mapped."));
+      }
+      this.getChildren().add(label);
+
+      label = new Label(" / ");
       label.setStyle("-fx-text-fill: #FFFFFF;");
-      label.setTooltip(new Tooltip("No VPS table mapped."));
-    }
-    this.getChildren().add(label);
+      this.getChildren().add(label);
 
-    label = new Label(" / ");
-    label.setStyle("-fx-text-fill: #FFFFFF;");
-    this.getChildren().add(label);
+      label = new Label();
+      if (vpsTableVersion != null) {
+        FontIcon checkboxIcon = WidgetFactory.createCheckboxIcon();
+        checkboxIcon.setIconSize(iconSize);
+        label.setGraphic(checkboxIcon);
+        label.setTooltip(new Tooltip("VPS Table Version:\n" + vpsTableVersion.toString()));
+      }
+      else {
+        label.setText(" - ");
+        label.setStyle("-fx-text-fill: #FFFFFF;");
+        label.setTooltip(new Tooltip("No VPS table version mapped."));
+      }
+      this.getChildren().add(label);
 
-    label = new Label();
-    if (vpsTableVersion != null) {
-      FontIcon checkboxIcon = WidgetFactory.createCheckboxIcon();
-      checkboxIcon.setIconSize(iconSize);
-      label.setGraphic(checkboxIcon);
-      label.setTooltip(new Tooltip("VPS Table Version:\n" + vpsTableVersion.toString()));
-    }
-    else {
-      label.setText(" - ");
+      label = new Label(" / ");
       label.setStyle("-fx-text-fill: #FFFFFF;");
-      label.setTooltip(new Tooltip("No VPS table version mapped."));
-    }
-    this.getChildren().add(label);
+      this.getChildren().add(label);
 
-    label = new Label(" / ");
-    label.setStyle("-fx-text-fill: #FFFFFF;");
-    this.getChildren().add(label);
+      label = new Label();
 
-    label = new Label();
-
-    int changeCounter = 0;
-    if (updates != null && !updates.isEmpty() && vpsTable != null) {
-      StringBuilder builder = new StringBuilder();
-      List<VPSChange> changes = updates.getChanges();
-      for (VPSChange change : changes) {
-        if (isFiltered(uiSettings, change)) {
-          continue;
+      int changeCounter = 0;
+      if (updates != null && !updates.isEmpty() && vpsTable != null) {
+        StringBuilder builder = new StringBuilder();
+        List<VPSChange> changes = updates.getChanges();
+        for (VPSChange change : changes) {
+          if (isFiltered(uiSettings, change)) {
+            continue;
+          }
+          changeCounter++;
+          builder.append(change.toString(vpsTable));
+          builder.append("\n");
         }
-        changeCounter++;
-        builder.append(change.toString(vpsTable));
-        builder.append("\n");
+
+        if (changeCounter > 0) {
+          FontIcon updateIcon = WidgetFactory.createUpdateIcon();
+          label.setGraphic(updateIcon);
+
+          String tooltip = "The table or its assets have received updates:\n\n" + builder + "\n\nYou can reset this indicator with the reset action from the context menu.";
+          Tooltip tt = new Tooltip(tooltip);
+          tt.setStyle("-fx-font-weight: bold;");
+          tt.setWrapText(true);
+          tt.setMaxWidth(400);
+          label.setTooltip(tt);
+        }
       }
 
-      if (changeCounter > 0) {
-        FontIcon updateIcon = WidgetFactory.createUpdateIcon();
-        label.setGraphic(updateIcon);
-
-        String tooltip = "The table or its assets have received updates:\n\n" + builder + "\n\nYou can reset this indicator with the reset action from the context menu.";
-        Tooltip tt = new Tooltip(tooltip);
-        tt.setStyle("-fx-font-weight: bold;");
-        tt.setWrapText(true);
-        tt.setMaxWidth(400);
-        label.setTooltip(tt);
+      if (changeCounter == 0) {
+        label.setText(" - ");
+        label.setTooltip(new Tooltip("No updates available."));
+        label.setStyle("-fx-text-fill: #FFFFFF;");
       }
-    }
 
-    if (changeCounter == 0) {
-      label.setText(" - ");
-      label.setTooltip(new Tooltip("No updates available."));
-      label.setStyle("-fx-text-fill: #FFFFFF;");
+      this.getChildren().add(label);
     }
-
-    this.getChildren().add(label);
+    catch (Exception e) {
+      LOG.error("Failed to render VPS table container: " + e.getMessage(), e);
+      this.getChildren().add(new Label("ERROR"));
+    }
   }
 
   private static boolean isFiltered(UISettings uiSettings, VPSChange change) {
--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tournaments/VpsVersionContainer.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/tournaments/VpsVersionContainer.java
@@ -26,84 +26,90 @@ public class VpsVersionContainer extends VBox {
 
   public VpsVersionContainer(VpsTable vpsTable, VpsTableVersion item, String customStyle, boolean downloadAction) {
     super(3);
-    setPadding(new Insets(3));
-    setMinHeight(124);
-    setMaxHeight(124);
-    setMaxWidth(500);
+    try {
+      setPadding(new Insets(3));
+      setMinHeight(124);
+      setMaxHeight(124);
+      setMaxWidth(500);
 
-    String comment = item.getComment();
-    if (comment != null && !comment.trim().isEmpty()) {
-      Label title = new Label(comment);
-      title.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 14px;-fx-font-weight : bold;" + customStyle);
-      this.getChildren().add(title);
-    }
-
-    if (item.getAuthors() != null && item.getAuthors().size() > 0) {
-      String authors = String.join(", ", item.getAuthors());
-      Label title = new Label(authors);
-      title.getStyleClass().add("default-text");
-      title.setStyle(customStyle);
-      if (comment == null || comment.trim().isEmpty()) {
+      String comment = item.getComment();
+      if (comment != null && !comment.trim().isEmpty()) {
+        Label title = new Label(comment);
         title.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 14px;-fx-font-weight : bold;" + customStyle);
+        this.getChildren().add(title);
       }
-      this.getChildren().add(title);
-      if (comment == null || comment.trim().isEmpty()) {
-        Label spacer = new Label();
-        spacer.setStyle("-fx-font-size : 14px;");
-        this.getChildren().add(spacer);
-      }
-    }
 
-    HBox row = new HBox(6);
-    Label titleLabel = new Label("Version:");
-    titleLabel.setPrefWidth(TITLE_WIDTH);
-    titleLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;-fx-font-weight : bold;" + customStyle);
-    Label valueLabel = new Label(item.getVersion());
-    valueLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;" + customStyle);
-    row.getChildren().addAll(titleLabel, valueLabel);
-
-    this.getChildren().add(row);
-
-    row = new HBox(6);
-    titleLabel = new Label("VPS Update:");
-    titleLabel.setPrefWidth(TITLE_WIDTH);
-    titleLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;-fx-font-weight : bold;" + customStyle);
-    valueLabel = new Label(DateFormat.getDateInstance().format(new Date(item.getUpdatedAt())));
-    valueLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;" + customStyle);
-    row.getChildren().addAll(titleLabel, valueLabel);
-    this.getChildren().add(row);
-
-    row = new HBox(6);
-    Button button = new Button("Download Table");
-
-    if (downloadAction && vpsTable != null) {
-      button.setOnAction(event -> {
-        Studio.browse(VPS.getVpsTableUrl(vpsTable.getId()));
-      });
-
-      FontIcon icon = new FontIcon("mdi2o-open-in-new");
-      icon.setIconSize(8);
-      icon.setIconColor(Paint.valueOf("#FFFFFF"));
-      button.setGraphic(icon);
-      button.setStyle("-fx-font-size: 12px;");
-      button.getStyleClass().add("external-component");
-      row.getChildren().add(button);
-    }
-    else {
-      List<String> features = item.getFeatures();
-      if (features != null) {
-        for (String feature : features) {
-          Label badge = new Label(feature);
-          badge.getStyleClass().add("white-label");
-          badge.setTooltip(new Tooltip(VpsUtil.getFeatureColorTooltip(feature)));
-          badge.getStyleClass().add("vps-badge");
-          badge.setStyle("-fx-background-color: " + VpsUtil.getFeatureColor(feature) + ";");
-          row.getChildren().add(badge);
+      if (item.getAuthors() != null && item.getAuthors().size() > 0) {
+        String authors = String.join(", ", item.getAuthors());
+        Label title = new Label(authors);
+        title.getStyleClass().add("default-text");
+        title.setStyle(customStyle);
+        if (comment == null || comment.trim().isEmpty()) {
+          title.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 14px;-fx-font-weight : bold;" + customStyle);
+        }
+        this.getChildren().add(title);
+        if (comment == null || comment.trim().isEmpty()) {
+          Label spacer = new Label();
+          spacer.setStyle("-fx-font-size : 14px;");
+          this.getChildren().add(spacer);
         }
       }
-    }
-    this.getChildren().add(row);
 
-    row.setPadding(new Insets(3, 0, 6, 0));
+      HBox row = new HBox(6);
+      Label titleLabel = new Label("Version:");
+      titleLabel.setPrefWidth(TITLE_WIDTH);
+      titleLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;-fx-font-weight : bold;" + customStyle);
+      Label valueLabel = new Label(item.getVersion());
+      valueLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;" + customStyle);
+      row.getChildren().addAll(titleLabel, valueLabel);
+
+      this.getChildren().add(row);
+
+      row = new HBox(6);
+      titleLabel = new Label("VPS Update:");
+      titleLabel.setPrefWidth(TITLE_WIDTH);
+      titleLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;-fx-font-weight : bold;" + customStyle);
+      valueLabel = new Label(DateFormat.getDateInstance().format(new Date(item.getUpdatedAt())));
+      valueLabel.setStyle("-fx-text-fill: #FFFFFF;-fx-font-size : 12px;" + customStyle);
+      row.getChildren().addAll(titleLabel, valueLabel);
+      this.getChildren().add(row);
+
+      row = new HBox(6);
+      Button button = new Button("Download Table");
+
+      if (downloadAction && vpsTable != null) {
+        button.setOnAction(event -> {
+          Studio.browse(VPS.getVpsTableUrl(vpsTable.getId()));
+        });
+
+        FontIcon icon = new FontIcon("mdi2o-open-in-new");
+        icon.setIconSize(8);
+        icon.setIconColor(Paint.valueOf("#FFFFFF"));
+        button.setGraphic(icon);
+        button.setStyle("-fx-font-size: 12px;");
+        button.getStyleClass().add("external-component");
+        row.getChildren().add(button);
+      }
+      else {
+        List<String> features = item.getFeatures();
+        if (features != null) {
+          for (String feature : features) {
+            Label badge = new Label(feature);
+            badge.getStyleClass().add("white-label");
+            badge.setTooltip(new Tooltip(VpsUtil.getFeatureColorTooltip(feature)));
+            badge.getStyleClass().add("vps-badge");
+            badge.setStyle("-fx-background-color: " + VpsUtil.getFeatureColor(feature) + ";");
+            row.getChildren().add(badge);
+          }
+        }
+      }
+      this.getChildren().add(row);
+
+      row.setPadding(new Insets(3, 0, 6, 0));
+    }
+    catch (Exception e) {
+      LOG.error("Failed to render VPS version container: " + e.getMessage(), e);
+      this.getChildren().add(new Label("ERROR"));
+    }
   }
 }
