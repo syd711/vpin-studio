bfdf9d6a0877343703767006cb93ccbf77871179
syd711 <syd711@gmail.com>
2024-08-08 16:54:39 +0200

    backglass image extraction

--- vpin-studio-server/src/main/java/de/mephisto/vpin/server/directb2s/DirectB2SImageExporter.java
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/directb2s/DirectB2SImageExporter.java
@@ -1,6 +1,5 @@
 package de.mephisto.vpin.server.directb2s;
 
-import de.mephisto.vpin.restclient.directb2s.DirectB2SData;
 import de.mephisto.vpin.server.VPinStudioException;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.commons.io.IOUtils;
