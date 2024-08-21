2847585c58ed6633ce2ef090cdf2554a7af9fe3b
Matthias Faust <syd711@gmail.com>
2024-08-21 19:36:25 +0200

    Fixed shutdown reset

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/preferences/InputPreferencesController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/preferences/InputPreferencesController.java
@@ -2,12 +2,11 @@ package de.mephisto.vpin.ui.preferences;
 
 import de.mephisto.vpin.restclient.PreferenceNames;
 import de.mephisto.vpin.restclient.preferences.PauseMenuSettings;
+import de.mephisto.vpin.ui.PreferencesController;
 import de.mephisto.vpin.ui.preferences.dialogs.PreferencesDialogs;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +22,12 @@ public class InputPreferencesController implements Initializable {
   @FXML
   private Button recordBtn;
 
+//  @FXML
+//  private CheckBox ignoreInputs;
+
+  @FXML
+  private TextField filterListText;
+
   @FXML
   private Spinner<Integer> inputDebounceSpinner;
 
@@ -34,8 +39,11 @@ public class InputPreferencesController implements Initializable {
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    recordBtn.setDisable(!client.getSystemService().isLocal());
     PauseMenuSettings pauseMenuSettings = client.getPreferenceService().getJsonPreference(PreferenceNames.PAUSE_MENU_SETTINGS, PauseMenuSettings.class);
+
+//    recordBtn.setDisable(!client.getSystemService().isLocal() || pauseMenuSettings.isIgnoreInputs());
+//    filterListText.setDisable(pauseMenuSettings.isIgnoreInputs());
+//    inputDebounceSpinner.setDisable(pauseMenuSettings.isIgnoreInputs());
 
     SpinnerValueFactory.IntegerSpinnerValueFactory factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100000, pauseMenuSettings.getInputDebounceMs());
     factory.setAmountToStepBy(100);
@@ -45,5 +53,19 @@ public class InputPreferencesController implements Initializable {
       pauseMenuSettings.setInputDebounceMs(value1);
       client.getPreferenceService().setJsonPreference(PreferenceNames.PAUSE_MENU_SETTINGS, pauseMenuSettings);
     }, 1000));
+
+//    ignoreInputs.setSelected(pauseMenuSettings.isIgnoreInputs());
+//    ignoreInputs.selectedProperty().addListener((observableValue, aBoolean, t1) -> {
+//      pauseMenuSettings.setIgnoreInputs(t1);
+//      filterListText.setDisable(t1);
+//      recordBtn.setDisable(t1);
+//      inputDebounceSpinner.setDisable(t1);
+//      client.getPreferenceService().setJsonPreference(PreferenceNames.PAUSE_MENU_SETTINGS, pauseMenuSettings);
+//    });
+
+    filterListText.textProperty().addListener((observableValue, s, t1) -> debouncer.debounce("filterList", () -> {
+      pauseMenuSettings.setInputFilterList(t1);
+      client.getPreferenceService().setJsonPreference(PreferenceNames.PAUSE_MENU_SETTINGS, pauseMenuSettings);
+    }, 500));
   }
 }
