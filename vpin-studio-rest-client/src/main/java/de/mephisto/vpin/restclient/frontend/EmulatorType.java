534572eeb3ef3f2a22d5503d1d20dd548fed59c4
Matthias Faust <syd711@gmail.com>
2024-10-18 13:04:29 +0200

    Changed emulator detections

--- vpin-studio-rest-client/src/main/java/de/mephisto/vpin/restclient/frontend/EmulatorType.java
+++ vpin-studio-rest-client/src/main/java/de/mephisto/vpin/restclient/frontend/EmulatorType.java
@@ -1,16 +1,12 @@
 package de.mephisto.vpin.restclient.frontend;
 
-import java.io.File;
-
-import org.apache.commons.io.FilenameUtils;
-
 public enum EmulatorType {
 
-  VisualPinball("vpx"), VisualPinball9("vpt"), 
-  FuturePinball("fpt"), 
+  VisualPinball("vpx"), VisualPinball9("vpt"),
+  FuturePinball("fpt"),
   ZenFX(null), ZenFX2("pxp"), ZenFX3("pxp"), PinballM(null),
-  Zaccaria(null), 
-  PinballArcade(null), 
+  Zaccaria(null),
+  PinballArcade(null),
   OTHER(null);
 
   private String extension;
@@ -34,37 +30,41 @@ public enum EmulatorType {
   public boolean isFxEmulator() {
     return ZenFX.equals(this) || ZenFX2.equals(this) || ZenFX3.equals(this) || PinballM.equals(this);
   }
-  
+
   public static EmulatorType fromName(String emuName) {
     if (emuName == null) {
       return null;
     }
-    
+
     String emu = emuName.replaceAll(" ", "").toLowerCase();
     if (emu.startsWith("visualpinball")) {
       return VisualPinball;
-    } 
+    }
     else if (emu.startsWith("futurepinball")) {
-        return FuturePinball;
+      return FuturePinball;
     }
     else if (emu.startsWith("pinballfx2")) {
-        return ZenFX2;
+      return ZenFX2;
     }
     else if (emu.startsWith("pinballfx3")) {
-        return ZenFX3;
+      return ZenFX3;
     }
     else if (emu.startsWith("zaccaria")) {
       return Zaccaria;
-  }
-  else if (emu.startsWith("pinballarcade")) {
-        return PinballArcade;
+    }
+    else if (emu.startsWith("pinballarcade")) {
+      return PinballArcade;
     }
     else {
-        return null;
+      return null;
     }
   }
 
   public static EmulatorType fromExtension(String extension) {
+    if (extension == null) {
+      return null;
+    }
+
     for (EmulatorType type : EmulatorType.values()) {
       if (type.getExtension() != null && type.getExtension().equalsIgnoreCase(extension)) {
         return type;
