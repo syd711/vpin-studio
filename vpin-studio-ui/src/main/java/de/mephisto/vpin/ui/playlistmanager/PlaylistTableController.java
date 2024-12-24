a127d33fb26f0ba37b05352a1de6a9e2bf15ca11
Matthias Faust <syd711@gmail.com>
2024-12-24 11:08:50 +0100

    Playlist Manager

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/playlistmanager/PlaylistTableController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/playlistmanager/PlaylistTableController.java
@@ -1,23 +1,27 @@
 package de.mephisto.vpin.ui.playlistmanager;
 
+import de.mephisto.vpin.restclient.PreferenceNames;
 import de.mephisto.vpin.restclient.frontend.PlaylistGame;
+import de.mephisto.vpin.restclient.games.GameEmulatorRepresentation;
 import de.mephisto.vpin.restclient.games.GameRepresentation;
 import de.mephisto.vpin.restclient.games.PlaylistRepresentation;
+import de.mephisto.vpin.restclient.preferences.UISettings;
 import de.mephisto.vpin.ui.tables.GameRepresentationModel;
 import de.mephisto.vpin.ui.tables.panels.BaseLoadingColumn;
 import de.mephisto.vpin.ui.tables.panels.BaseTableController;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
@@ -36,7 +40,21 @@ public class PlaylistTableController extends BaseTableController<GameRepresentat
   @FXML
   TableColumn<GameRepresentationModel, GameRepresentationModel> columnEmulator;
 
+  @FXML
+  private ComboBox<GameEmulatorRepresentation> emulatorCombo;
+
   private Stage stage;
+
+
+  @FXML
+  private void onRemove() {
+
+  }
+
+  @FXML
+  private void onTableMouseClicked(MouseEvent e) {
+
+  }
 
   public void setData(Stage dialogStage, Optional<PlaylistRepresentation> value) {
     if (value.isPresent()) {
@@ -48,14 +66,11 @@ public class PlaylistTableController extends BaseTableController<GameRepresentat
     return client.getGameService().getGameCached(playlistGame.getId());
   }
 
-  @FXML
-  private void onTableMouseClicked(MouseEvent e) {
-
-  }
-
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     super.initialize("media", "media", new PlaylistTableColumnSorter(this));
+
+    tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
     BaseLoadingColumn.configureColumn(columnName, (value, model) -> {
       Label label = new Label(model.getName());
@@ -71,6 +86,10 @@ public class PlaylistTableController extends BaseTableController<GameRepresentat
       label.setText(model.getGameEmulator().getName());
       return label;
     }, true);
+
+    UISettings uiSettings = client.getPreferenceService().getJsonPreference(PreferenceNames.UI_SETTINGS, UISettings.class);
+    List<GameEmulatorRepresentation> filtered = new ArrayList<>(client.getFrontendService().getFilteredEmulatorsWithAllVpx(uiSettings));
+    this.emulatorCombo.setItems(FXCollections.observableList(filtered));
   }
 
   @Override
