138d46b2263176bf5e50dfb3529555963e3f72da
Matthias Faust <syd711@gmail.com>
2025-01-22 19:26:58 +0100

    server ui

---
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/security/ResourceHandlers.java
@@ -0,0 +1,23 @@
+package de.mephisto.vpin.server.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class ResourceHandlers extends WebMvcConfigurerAdapter {
+
+  @Value("${server.resources.folder}")
+  private String resourcesFolder;
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/ui/**")
+        .addResourceLocations(resourcesFolder);
+
+  }
+}
