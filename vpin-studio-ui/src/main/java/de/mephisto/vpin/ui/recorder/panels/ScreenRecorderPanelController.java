661a1398b7435ee161245bb59ea4520aae5a6f7e
gonzonia <36857621+gonzonia@users.noreply.github.com>
2025-02-07 16:39:05 -0500

    Revert "Merge branch 'main' of https://github.com/gonzonia/vpin-studio"
    This reverts commit 65edfa88cfd7a39b7cee0bbd1982ce7e9c4a7fc4, reversing
    changes made to 94efbdd7eef168062fa9795c7cd84ece5e79d17e.

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/recorder/panels/ScreenRecorderPanelController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/recorder/panels/ScreenRecorderPanelController.java
@@ -18,7 +18,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +28,6 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import static de.mephisto.vpin.ui.Studio.client;
-import static de.mephisto.vpin.ui.Studio.stage;
 import static de.mephisto.vpin.ui.util.PreferenceBindingUtil.debouncer;
 
 public class ScreenRecorderPanelController implements Initializable {
@@ -37,7 +35,6 @@ public class ScreenRecorderPanelController implements Initializable {
 
   private final static List<RecordingWriteMode> RECORD_MODE_LIST = Arrays.asList(RecordingWriteMode.ifMissing, RecordingWriteMode.overwrite, RecordingWriteMode.append);
   public static final int PREVIEW_WIDTH_THRESHOLD = 1600;
-
 
   @FXML
   private Pane root;
@@ -50,9 +47,6 @@ public class ScreenRecorderPanelController implements Initializable {
 
   @FXML
   private ImageView imageView;
-
-  @FXML
-  private AnchorPane previewAnchor;
 
   @FXML
   private Label screenName;
@@ -106,9 +100,6 @@ public class ScreenRecorderPanelController implements Initializable {
         }, 200);
       }
     });
-
-    imageView.fitWidthProperty().bind(previewAnchor.widthProperty());
-    imageView.fitHeightProperty().bind(previewAnchor.heightProperty());
 
     this.recordingScreen = recordingScreen;
     screenName.setText(recordingScreen.getScreen().name());
@@ -231,12 +222,8 @@ public class ScreenRecorderPanelController implements Initializable {
       preview.setPrefWidth(w);
       preview.setPrefHeight(h * 8 / 16);
 
-      //imageView.setFitWidth(width);
-      //imageView.setFitHeight(height);
-
-      previewAnchor.setMaxHeight(height);
-      previewAnchor.setMaxWidth(width);
-
+      imageView.setFitWidth(width);
+      imageView.setFitHeight(height);
 
       Image image = MonitoringManager.getInstance().getRecordableScreenImage(recordingScreen);
       imageView.setImage(image);
