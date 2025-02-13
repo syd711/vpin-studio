22493c6129b646141e046d1658eff9836473426d
Matthias Faust <syd711@gmail.com>
2025-02-13 08:27:44 +0100

    qa

--- vpin-studio-server/src/main/java/de/mephisto/vpin/server/players/PlayerService.java
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/players/PlayerService.java
@@ -111,8 +111,9 @@ public class PlayerService {
 
   public Player save(Player player) {
     Player model = new Player();
-    boolean existingPlayer = model.getId() > 0;
-    if (existingPlayer) {
+    boolean existingPlayer = false;
+    if (player.getId() != null && player.getId() > 0) {
+      existingPlayer = true;
       model = playerRepository.findById(player.getId()).get();
     }
 
