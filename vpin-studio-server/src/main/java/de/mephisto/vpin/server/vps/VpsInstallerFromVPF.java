730476ef08107d0e12d7f89ac764b959977b8293
Matthias Faust <syd711@gmail.com>
2025-08-03 17:27:38 +0200

    installers restore

--- vpin-studio-server/src/main/java/de/mephisto/vpin/server/vps/VpsInstallerFromVPF.java
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/vps/VpsInstallerFromVPF.java
@@ -3,15 +3,12 @@ package de.mephisto.vpin.server.vps;
 import de.mephisto.vpin.restclient.vpf.VPFSettings;
 import de.mephisto.vpin.restclient.vps.VpsInstallLink;
 import de.mephisto.vpin.server.vpauthenticators.VpfVPAuthenticator;
+import de.mephisto.vpin.server.vpauthenticators.VpuVPAuthenticator;
 import org.apache.commons.lang3.StringUtils;
-import org.htmlunit.Page;
-import org.htmlunit.UnexpectedPage;
-import org.htmlunit.WebClient;
-import org.htmlunit.WebWindow;
-import org.htmlunit.html.DomNode;
-import org.htmlunit.html.HtmlAnchor;
-import org.htmlunit.html.HtmlElement;
-import org.htmlunit.html.HtmlPage;
+import org.htmlunit.*;
+import org.htmlunit.html.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +18,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
 public class VpsInstallerFromVPF implements VpsInstaller {
+  private final static Logger LOG = LoggerFactory.getLogger(VpsInstallerFromVPF.class);
 
   private final VPFSettings settings;
 
@@ -30,93 +28,131 @@ public class VpsInstallerFromVPF implements VpsInstaller {
 
   @Override
   public String login() throws IOException {
-    return new VpfVPAuthenticator(this.settings).login();
+    try (final WebClient webClient = new WebClient()) {
+      webClient.setCssErrorHandler(new SilentCssErrorHandler());
+      webClient.getOptions().setThrowExceptionOnScriptError(false);
+      webClient.getOptions().setJavaScriptEnabled(false);
+
+      return doLogin(webClient);
+    }
+  }
+
+  private String doLogin(final WebClient webClient) {
+    try {
+      // don't even try to authenticate if settings are not set
+      if (StringUtils.isBlank(settings.getLogin())) {
+        return "Login cannot be empty";
+      }
+
+      // Perfom VPF authentication
+      HtmlPage loginPage = webClient.getPage("https://www.vpforums.org/index.php?app=core&module=global&section=login");
+      HtmlForm loginForm = loginPage.getForms().stream()
+          .filter(f -> StringUtils.equalsIgnoreCase(f.getId(), "login"))
+          .findFirst().orElseThrow();
+      loginForm.getInputByName("ips_username").setValue(settings.getLogin());
+      loginForm.getInputByName("ips_password").setValue(settings.getPassword());
+      DomElement submitBtn = loginPage.querySelector("input.input_submit");
+      HtmlPage homePage = submitBtn.click();
+
+      // check authentication happens correctly
+      String title = homePage.getTitleText();
+      if (StringUtils.containsIgnoreCase(title, "sign in")) {
+        DomNode node = homePage.querySelector("p.message.error");
+        return node != null ? node.getTextContent().trim() : "Cannot login";
+      }
+      // login successful, no error
+      return null;
+    }
+    catch (IOException e) {
+      LOG.error("Login failed: {}", e.getMessage(), e);
+      return "Login failed: " + e.getMessage();
+    }
   }
 
   @Override
   public void browse(String link, BiConsumer<VpsInstallLink, Supplier<InputStream>> consumer) throws IOException {
-//    try (final WebClient webClient = new WebClient()) {
-//      webClient.getOptions().setThrowExceptionOnScriptError(false);
-//      webClient.getOptions().setJavaScriptEnabled(false);
-//
-//      if (login(webClient) == null) {
-//        // Access the page of the link and click on the download button
-//        HtmlPage downloadPage = webClient.getPage(link);
-//        HtmlAnchor linkBtn = downloadPage.querySelector("a.download_button");
-//
-//        Page page = linkBtn.click();
-//        if (page instanceof UnexpectedPage) {
-//          UnexpectedPage enclosedPage = (UnexpectedPage) page;
-//          String attname = enclosedPage.getWebResponse().getResponseHeaderValue("content-disposition");
-//          attname = StringUtils.substringBetween(attname, "\"", "\"");
-//
-//          String contentLength = enclosedPage.getWebResponse().getResponseHeaderValue("content-length");
-//          Double size = Double.parseDouble(contentLength) / 1024;
-//          String unit = "KB";
-//          if (size > 1024) {
-//            size /= 1024;
-//            unit = "MB";
-//          }
-//
-//          // some link goes though direct download
-//          VpsInstallLink l = new VpsInstallLink();
-//          l.setName(attname);
-//          l.setOrder(0);
-//          l.setSize(size + unit);
-//          l.setUrl(linkBtn.getHrefAttribute());
-//
-//          Supplier<InputStream> inputStreamSupplier = () -> {
-//            try {
-//              return enclosedPage.getInputStream();
-//            }
-//            catch (IOException ioe) {
-//              return null;
-//            }
-//          };
-//          consumer.accept(l, inputStreamSupplier);
-//          return;
-//        }
-//        // else go through agreement page
-//        HtmlPage agreePage = (HtmlPage) page;
-//        HtmlAnchor agreeBtn = agreePage.querySelector("a#agree_disclaimer");
-//        if (agreeBtn != null) {
-//          agreePage = agreeBtn.click();
-//        }
-//
-//        // display files
-//        HtmlPage linksPage = agreePage;
-//        List<DomNode> files = linksPage.querySelectorAll("#files li");
-//        int order = 0;
-//        for (DomNode file : files) {
-//          Iterator<HtmlElement> children = file.getHtmlElementDescendants().iterator();
-//
-//          String size = nextChild(children, linksPage).getTextContent().trim();
-//
-//          HtmlAnchor a = (HtmlAnchor) nextChild(children);
-//          String url = a != null ? a.getHrefAttribute() : null;
-//
-//          String name = nextChild(children, linksPage).getTextContent().trim();
-//
-//          VpsInstallLink l = new VpsInstallLink();
-//          l.setName(name);
-//          l.setOrder(order++);
-//          l.setSize(size);
-//          l.setUrl(url);
-//
-//          Supplier<InputStream> inputStreamSupplier = a == null ? null : () -> {
-//            try {
-//              a.click();
-//              WebWindow window = linksPage.getEnclosingWindow();
-//              UnexpectedPage enclosedPage = (UnexpectedPage) window.getEnclosedPage();
-//              return enclosedPage.getInputStream();
-//            }
-//            catch (IOException ioe) {
-//              return null;
-//            }
-//          };
-//          consumer.accept(l, inputStreamSupplier);
-//        }
-//      }
-//    }
+    try (final WebClient webClient = new WebClient()) {
+      webClient.getOptions().setThrowExceptionOnScriptError(false);
+      webClient.getOptions().setJavaScriptEnabled(false);
+
+      if (doLogin(webClient) == null) {
+        // Access the page of the link and click on the download button
+        HtmlPage downloadPage = webClient.getPage(link);
+        HtmlAnchor linkBtn = downloadPage.querySelector("a.download_button");
+
+        Page page = linkBtn.click();
+        if (page instanceof UnexpectedPage) {
+          UnexpectedPage enclosedPage = (UnexpectedPage) page;
+          String attname = enclosedPage.getWebResponse().getResponseHeaderValue("content-disposition");
+          attname = StringUtils.substringBetween(attname, "\"", "\"");
+
+          String contentLength = enclosedPage.getWebResponse().getResponseHeaderValue("content-length");
+          Double size = Double.parseDouble(contentLength) / 1024;
+          String unit = "KB";
+          if (size > 1024) {
+            size /= 1024;
+            unit = "MB";
+          }
+
+          // some link goes though direct download
+          VpsInstallLink l = new VpsInstallLink();
+          l.setName(attname);
+          l.setOrder(0);
+          l.setSize(size + unit);
+          l.setUrl(linkBtn.getHrefAttribute());
+
+          Supplier<InputStream> inputStreamSupplier = () -> {
+            try {
+              return enclosedPage.getInputStream();
+            }
+            catch (IOException ioe) {
+              return null;
+            }
+          };
+          consumer.accept(l, inputStreamSupplier);
+          return;
+        }
+        // else go through agreement page
+        HtmlPage agreePage = (HtmlPage) page;
+        HtmlAnchor agreeBtn = agreePage.querySelector("a#agree_disclaimer");
+        if (agreeBtn != null) {
+          agreePage = agreeBtn.click();
+        }
+
+        // display files
+        HtmlPage linksPage = agreePage;
+        List<DomNode> files = linksPage.querySelectorAll("#files li");
+        int order = 0;
+        for (DomNode file : files) {
+          Iterator<HtmlElement> children = file.getHtmlElementDescendants().iterator();
+
+          String size = nextChild(children, linksPage).getTextContent().trim();
+
+          HtmlAnchor a = (HtmlAnchor) nextChild(children);
+          String url = a != null ? a.getHrefAttribute() : null;
+
+          String name = nextChild(children, linksPage).getTextContent().trim();
+
+          VpsInstallLink l = new VpsInstallLink();
+          l.setName(name);
+          l.setOrder(order++);
+          l.setSize(size);
+          l.setUrl(url);
+
+          Supplier<InputStream> inputStreamSupplier = a == null ? null : () -> {
+            try {
+              a.click();
+              WebWindow window = linksPage.getEnclosingWindow();
+              UnexpectedPage enclosedPage = (UnexpectedPage) window.getEnclosedPage();
+              return enclosedPage.getInputStream();
+            }
+            catch (IOException ioe) {
+              return null;
+            }
+          };
+          consumer.accept(l, inputStreamSupplier);
+        }
+      }
+    }
   }
 }
--- vpin-studio-server/src/main/java/de/mephisto/vpin/server/vps/VpsInstallerFromVPU.java
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/vps/VpsInstallerFromVPU.java
@@ -9,18 +9,20 @@ import java.util.function.Supplier;
 
 import de.mephisto.vpin.server.vpauthenticators.VpfVPAuthenticator;
 import de.mephisto.vpin.server.vpauthenticators.VpuVPAuthenticator;
+import org.apache.commons.lang3.StringUtils;
+import org.htmlunit.SilentCssErrorHandler;
 import org.htmlunit.UnexpectedPage;
 import org.htmlunit.WebClient;
 import org.htmlunit.WebWindow;
-import org.htmlunit.html.DomNode;
-import org.htmlunit.html.HtmlAnchor;
-import org.htmlunit.html.HtmlElement;
-import org.htmlunit.html.HtmlPage;
+import org.htmlunit.html.*;
 
 import de.mephisto.vpin.restclient.vps.VpsInstallLink;
 import de.mephisto.vpin.restclient.vpu.VPUSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VpsInstallerFromVPU implements VpsInstaller {
+  private final static Logger LOG = LoggerFactory.getLogger(VpsInstallerFromVPU.class);
 
   private VPUSettings settings;
 
@@ -30,53 +32,90 @@ public class VpsInstallerFromVPU implements VpsInstaller {
 
   @Override
   public String login() throws IOException {
-    return new VpuVPAuthenticator(this.settings).login();
+    try (final WebClient webClient = new WebClient()) {
+      webClient.setCssErrorHandler(new SilentCssErrorHandler());
+      webClient.getOptions().setThrowExceptionOnScriptError(false);
+      webClient.getOptions().setJavaScriptEnabled(false);
+
+      return doLogin(webClient);
+    }
+  }
+
+  private String doLogin(final WebClient webClient) {
+    try {
+      // don't even try to authenticate if settings are not set
+      if (StringUtils.isBlank(settings.getLogin())) {
+        return "Login cannot be empty";
+      }
+
+      final HtmlPage loginPage = webClient.getPage("https://vpuniverse.com/login/");
+
+      HtmlForm loginForm = loginPage.getForms().stream()
+          .filter(f -> StringUtils.containsIgnoreCase(f.getActionAttribute(), "/login"))
+          .findFirst().orElseThrow();
+
+      loginForm.getInputByName("auth").setValue(settings.getLogin());
+      loginForm.getInputByName("password").setValue(settings.getPassword());
+      final HtmlPage homePage = loginForm.getButtonByName("_processLogin").click();
+
+      // check that authentication happens successfully
+      String title = homePage.getTitleText();
+      if (StringUtils.containsIgnoreCase(title, "sign in")) {
+        DomNode node = homePage.querySelector("div.ipsMessage_error");
+        return node != null ? node.getTextContent() : "Cannot login";
+      }
+      return null;
+    }
+    catch (IOException e) {
+      LOG.error("Login failed: {}", e.getMessage(), e);
+      return "Login failed: " + e.getMessage();
+    }
   }
 
   @Override
   public void browse(String link, BiConsumer<VpsInstallLink, Supplier<InputStream>> consumer) throws IOException {
-//    try (final WebClient webClient = new WebClient()) {
-//      webClient.getOptions().setThrowExceptionOnScriptError(false);
-//      webClient.getOptions().setJavaScriptEnabled(false);
-//
-//      if (doLogin(webClient) == null) {
-//
-//        HtmlPage linksPage = webClient.getPage(link + "?do=download");
-//
-//        List<DomNode> files = linksPage.querySelectorAll(".ipsDataItem");
-//        int order = 0;
-//        for (DomNode file : files) {
-//
-//          Iterator<HtmlElement> children = file.getHtmlElementDescendants().iterator();
-//          nextChild(children);
-//            nextChild(children);
-//              String name = nextChild(children, linksPage).getTextContent();
-//            String size = nextChild(children, linksPage).getTextContent();
-//          nextChild(children);
-//            nextChild(children);
-//              nextChild(children);
-//            HtmlAnchor a = (HtmlAnchor) nextChild(children);
-//
-//          VpsInstallLink l = new VpsInstallLink();
-//          l.setName(name);
-//          l.setOrder(order++);
-//          l.setSize(size);
-//          l.setUrl(a != null ? a.getHrefAttribute() : null);
-//
-//          Supplier<InputStream> inputStreamSupplier = a == null? null : () -> {
-//            try {
-//              a.click();
-//              WebWindow window = linksPage.getEnclosingWindow();
-//              UnexpectedPage enclosedPage = (UnexpectedPage) window.getEnclosedPage();
-//              return enclosedPage.getInputStream();
-//            } catch (IOException ioe) {
-//              return null;
-//            }
-//          };
-//
-//          consumer.accept(l, inputStreamSupplier);
-//        }
-//      }
-//    }
+    try (final WebClient webClient = new WebClient()) {
+      webClient.getOptions().setThrowExceptionOnScriptError(false);
+      webClient.getOptions().setJavaScriptEnabled(false);
+
+      if (doLogin(webClient) == null) {
+
+        HtmlPage linksPage = webClient.getPage(link + "?do=download");
+
+        List<DomNode> files = linksPage.querySelectorAll(".ipsDataItem");
+        int order = 0;
+        for (DomNode file : files) {
+
+          Iterator<HtmlElement> children = file.getHtmlElementDescendants().iterator();
+          nextChild(children);
+            nextChild(children);
+              String name = nextChild(children, linksPage).getTextContent();
+            String size = nextChild(children, linksPage).getTextContent();
+          nextChild(children);
+            nextChild(children);
+              nextChild(children);
+            HtmlAnchor a = (HtmlAnchor) nextChild(children);
+
+          VpsInstallLink l = new VpsInstallLink();
+          l.setName(name);
+          l.setOrder(order++);
+          l.setSize(size);
+          l.setUrl(a != null ? a.getHrefAttribute() : null);
+
+          Supplier<InputStream> inputStreamSupplier = a == null? null : () -> {
+            try {
+              a.click();
+              WebWindow window = linksPage.getEnclosingWindow();
+              UnexpectedPage enclosedPage = (UnexpectedPage) window.getEnclosedPage();
+              return enclosedPage.getInputStream();
+            } catch (IOException ioe) {
+              return null;
+            }
+          };
+
+          consumer.accept(l, inputStreamSupplier);
+        }
+      }
+    }
   }
 }
