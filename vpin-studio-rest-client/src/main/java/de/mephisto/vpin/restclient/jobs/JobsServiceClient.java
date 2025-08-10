86d1895adf93042e95bd8f68b40ba134baeff2f7
Matthias Faust <syd711@gmail.com>
2025-08-10 14:05:50 +0200

    improved error handling

--- vpin-studio-rest-client/src/main/java/de/mephisto/vpin/restclient/jobs/JobsServiceClient.java
+++ vpin-studio-rest-client/src/main/java/de/mephisto/vpin/restclient/jobs/JobsServiceClient.java
@@ -3,21 +3,32 @@ package de.mephisto.vpin.restclient.jobs;
 import de.mephisto.vpin.restclient.client.VPinStudioClient;
 import de.mephisto.vpin.restclient.client.VPinStudioClientService;
 import de.mephisto.vpin.restclient.games.descriptors.JobDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /*********************************************************************************************************************
  * Jobs
  ********************************************************************************************************************/
 public class JobsServiceClient extends VPinStudioClientService {
+  private final static Logger LOG = LoggerFactory.getLogger(JobsServiceClient.class);
+
   public JobsServiceClient(VPinStudioClient client) {
     super(client);
   }
 
   public List<JobDescriptor> getJobs() {
-    return Arrays.asList(getRestClient().get(API + "jobs", JobDescriptor[].class));
+    try {
+      return Arrays.asList(getRestClient().get(API + "jobs", JobDescriptor[].class));
+    }
+    catch (Exception e) {
+      LOG.error("Failed to read jobs: {}", e.getMessage(), e);
+    }
+    return Collections.emptyList();
   }
 
   public JobDescriptor getJob(String uuid) {
@@ -31,6 +42,7 @@ public class JobsServiceClient extends VPinStudioClientService {
   public boolean dismiss(@PathVariable("uuid") String uuid) {
     return getRestClient().get(API + "jobs/dismiss/" + uuid, Boolean.class);
   }
+
   public boolean cancel(@PathVariable("uuid") String uuid) {
     return getRestClient().get(API + "jobs/cancel/" + uuid, Boolean.class);
   }
