aabc2383e8adf94d40ce6c52bd1fbfbfdcfde0b9
Matthias Faust <syd711@gmail.com>
2025-11-02 13:24:44 +0100

    dof tester

--- vpin-studio-server/src/main/java/de/mephisto/vpin/server/ServerUpdatePreProcessing.java
+++ vpin-studio-server/src/main/java/de/mephisto/vpin/server/ServerUpdatePreProcessing.java
@@ -42,6 +42,7 @@ public class ServerUpdatePreProcessing {
         runPinVolUpdateCheck();
         runVpxToolsUpdateCheck();
         runLogosUpdateCheck();
+        runDOFTesterCheck();
 
         new Thread(() -> {
           Thread.currentThread().setName("ServerUpdate Async Preprocessor");
@@ -94,6 +95,20 @@ public class ServerUpdatePreProcessing {
     }
   }
 
+  private static void runDOFTesterCheck() {
+    File testerFolder = new File(RESOURCES, "DOFTest");
+    if (!testerFolder.exists()) {
+      testerFolder.mkdirs();
+    }
+    List<String> dofTesterFileNames = Arrays.asList("DirectOutput.dll", "DirectOutputComObject.dll", "DirectOutputTest.exe", "DirectOutputTest.exe.config", "DirectOutputTest.pdb", "Readme.txt");
+    for (String dofTesterFileName : dofTesterFileNames) {
+      File check = new File(testerFolder, dofTesterFileName);
+      if (!check.exists()) {
+        LOG.info("Outdated {} found, updating...", check.getName());
+        Updater.download("https://raw.githubusercontent.com/syd711/vpin-studio/main/resources/DOFTest/" + dofTesterFileName, check);
+      }
+    }
+  }
 
   private static void runLogosUpdateCheck() {
     long expectedSize = 119644;
