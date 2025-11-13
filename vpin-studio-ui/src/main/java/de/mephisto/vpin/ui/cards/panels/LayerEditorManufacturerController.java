cbd9aa8354afdd87d3e34fe15836ff8d92d3acdb
syd711 <syd711@gmail.com>
2025-11-13 14:59:14 +0100

    logo AR

--- vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/cards/panels/LayerEditorManufacturerController.java
+++ vpin-studio-ui/src/main/java/de/mephisto/vpin/ui/cards/panels/LayerEditorManufacturerController.java
@@ -4,6 +4,8 @@ import de.mephisto.vpin.restclient.cards.CardTemplate;
 import de.mephisto.vpin.restclient.cards.CardResolution;
 import de.mephisto.vpin.restclient.games.GameRepresentation;
 import de.mephisto.vpin.ui.util.PositionResizer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
@@ -16,12 +18,13 @@ import static de.mephisto.vpin.ui.Studio.client;
 public class LayerEditorManufacturerController extends LayerEditorBaseController {
 
   @FXML
-  private CheckBox manufacturerLogoUseYearCheckBox;
+  private CheckBox manufacturerLogoUseHistoricalCheckBox;
   @FXML
   private CheckBox manufacturerLogoKeepARCheckBox;
 
   private CardTemplate cardTemplate;
   private Optional<GameRepresentation> game;
+  private Long aspectRatio = null;
 
   public void setTemplate(CardTemplate cardTemplate, CardResolution res, Optional<GameRepresentation> game) {
     this.cardTemplate = cardTemplate;
@@ -31,7 +34,7 @@ public class LayerEditorManufacturerController extends LayerEditorBaseController
 
     positionController.setTemplate("manufacturerLogo", cardTemplate, res);
 
-    manufacturerLogoUseYearCheckBox.setSelected(cardTemplate.isManufacturerLogoUseYear());
+    manufacturerLogoUseHistoricalCheckBox.setSelected(cardTemplate.isManufacturerLogoUseYear());
     manufacturerLogoKeepARCheckBox.setSelected(cardTemplate.isManufacturerLogoKeepAspectRatio());
 
   }
@@ -43,18 +46,36 @@ public class LayerEditorManufacturerController extends LayerEditorBaseController
 
     positionController.initBindings("manufacturerLogo", templateBeanBinder);
 
-    templateBeanBinder.bindCheckbox(manufacturerLogoUseYearCheckBox, "manufacturerLogoUseYear");
+    manufacturerLogoKeepARCheckBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+      @Override
+      public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        refreshAR();
+      }
+    });
+    manufacturerLogoUseHistoricalCheckBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+      @Override
+      public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        refreshAR();
+      }
+    });
+
+    templateBeanBinder.bindCheckbox(manufacturerLogoUseHistoricalCheckBox, "manufacturerLogoUseYear");
     templateBeanBinder.bindCheckbox(manufacturerLogoKeepARCheckBox, "manufacturerLogoKeepAspectRatio");
+  }
+
+  private void refreshAR() {
+    this.aspectRatio = null;
+    if (manufacturerLogoKeepARCheckBox.isSelected() && game.isPresent()) {
+      byte[] manufacturerLogos = client.getHighscoreCardsService().getHighscoreImage(game.get(), cardTemplate, "manufacturerLogo");
+      Image image = new Image(new ByteArrayInputStream(manufacturerLogos));
+      this.aspectRatio = Math.round(image.getWidth() / image.getHeight());
+    }
+    templateEditorController.selectLayer(this);
   }
 
   @Override
   public void bindDragBox(PositionResizer dragBox) {
-    if (game.isPresent()) {
-      byte[] manufacturerLogos = client.getHighscoreCardsService().getHighscoreImage(game.get(), cardTemplate, "manufacturerLogo");
-      Image image = new Image(new ByteArrayInputStream(manufacturerLogos));
-      double ratio = image.getWidth() / image.getHeight();
-      dragBox.setAspectRatio(ratio);
-    }
+    dragBox.setAspectRatio(aspectRatio != null ? aspectRatio.doubleValue() : null);
     positionController.bindDragBox(dragBox);
   }
 
