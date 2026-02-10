86db750b9415afd2545203575833a46778fd6974
Matthias Faust <syd711@gmail.com>
2026-02-10 18:26:37 +0100

    added dmddevice.ini

--- vpin-connector-github/src/main/java/de/mephisto/vpin/connectors/github/ArchiveHandler.java
+++ vpin-connector-github/src/main/java/de/mephisto/vpin/connectors/github/ArchiveHandler.java
@@ -10,6 +10,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -38,7 +39,7 @@ public class ArchiveHandler {
     this.installLog = installLog;
     this.rootFileIndicators = rootFileIndicators;
     this.includedFiles = includedFiles;
-    this.excludedFiles = excludedFiles;
+    this.excludedFiles = excludedFiles.stream().map(f -> f.toLowerCase()).collect(Collectors.toList());
   }
 
   public void diff(@NonNull File destinationDir) {
@@ -233,12 +234,12 @@ public class ArchiveHandler {
       }
     }
 
-    if (this.excludedFiles.contains(name)) {
+    if (this.excludedFiles.contains(name.toLowerCase())) {
       return true;
     }
 
     for (String excludedFile : this.excludedFiles) {
-      if (excludedFile.startsWith(".") && name.endsWith(excludedFile)) {
+      if (excludedFile.startsWith(".") && name.toLowerCase().endsWith(excludedFile)) {
         return true;
       }
     }
